04a4eeebc93033fffbce99610aa6f37cdbb42d06
Aliaksei Kalennikau <aliaksei.k@qawerk.com>
2022-11-16 15:51:30 +0100

    Add tests for dashboard (1st batch)

--- fixtures.js
+++ fixtures.js
@@ -11,10 +11,25 @@ const mainTest = base.test.extend({
     await loginPage.clickLoginButton();
     const dashboardPage = new DashboardPage(page);
     await dashboardPage.isHeaderDisplayed("Projects");
-    await dashboardPage.deleteFileIfExists();
+    await dashboardPage.deleteFilesIfExist();
     await dashboardPage.createFile();
     await use(page);
   },
 });
 
+const dashboardTest = base.test.extend({
+  page: async ({ page }, use) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.enterEmail(process.env.LOGIN_EMAIL);
+    await loginPage.enterPwd(process.env.LOGIN_PWD);
+    await loginPage.clickLoginButton();
+    const dashboardPage = new DashboardPage(page);
+    await dashboardPage.isHeaderDisplayed("Projects");
+    await dashboardPage.deleteFilesIfExist();
+    await use(page);
+  },
+});
+
 exports.mainTest = mainTest;
+exports.dashboardTest = dashboardTest;
--- pages/dashboard-page.js
+++ pages/dashboard-page.js
@@ -5,14 +5,43 @@ exports.DashboardPage = class DashboardPage {
    * @param {import('@playwright/test').Page} page
    */
   constructor(page) {
+    this.page = page;
     this.header = page.locator("h1");
     this.numberOfFilesText = page.locator(
       'div[class="project-name-wrapper"] span[class="info"]'
     );
     this.fileTile = page.locator('div[class="item-info"]');
+    this.fileNameTitle = page.locator('div[class="item-info"] h3');
     this.deleteFileMenuItem = page.locator('a[data-test="file-delete"]');
-    this.deletesFileButton = page.locator('input[value="Delete files"]');
+    this.deleteFileButton = page.locator(
+      'input[value="Delete files"],input[value="Delete file"]'
+    );
     this.createFileButton = page.locator('button[class="create-new"]');
+    this.renameFileMenuItem = page.locator('a[data-test="file-rename"]');
+    this.duplicateFileMenuItem = page.locator('a[data-test="file-duplicate"]');
+    this.addFileAsSharedLibraryMenuItem = page.locator(
+      'a[data-test="file-add-shared"]'
+    );
+    this.addFileAsSharedLibraryButton = page.locator(
+      'input[value="Add as Shared Library"]'
+    );
+    this.sharedLibraryIcon = page.locator('svg[class="icon-library"]');
+    this.delFileAsSharedLibraryMenuItem = page.locator(
+      'a[data-test="file-del-shared"]'
+    );
+    this.delFileAsSharedLibraryButton = page.locator(
+      'input[value="Unpublish"]'
+    );
+    this.downloadFilePenpot = page.locator(
+      'a[data-test="download-binary-file"]'
+    );
+    this.downloadFileStandard = page.locator(
+      'a[data-test="download-standard-file"]'
+    );
+    this.downloadFileTickIcon = page.locator('svg[class="icon-tick"]');
+    this.downloadFileCloseButton = page.locator('input[value="Close"]');
+    this.fileNameInput = page.locator('div[class="edit-wrapper"]');
+    this.successMessage = page.locator('div[class="banner success fixed"]');
   }
 
   async isHeaderDisplayed(title) {
@@ -27,13 +56,85 @@ exports.DashboardPage = class DashboardPage {
   async deleteFile() {
     await this.fileTile.click({ button: "right" });
     await this.deleteFileMenuItem.click();
-    await this.deletesFileButton.click();
+    await this.deleteFileButton.click();
   }
 
-  async deleteFileIfExists() {
+  async deleteFiles() {
+    for (const el of await this.fileTile.elementHandles()) {
+      await el.click({ button: "right" });
+      await this.deleteFileMenuItem.click();
+      await this.deleteFileButton.click();
+    }
+  }
+
+  async deleteFilesIfExist() {
     const text = (await this.numberOfFilesText.innerText()).valueOf();
     if (!text.includes("0 files")) {
-      await this.deleteFile();
+      await this.deleteFiles();
     }
+  }
+
+  async waitForPageLoaded() {
+    await this.page.waitForLoadState("networkidle");
+  }
+
+  async checkNumberOfFiles(numberOfFiles) {
+    await this.waitForPageLoaded();
+    const text = (await this.numberOfFilesText.innerText()).valueOf();
+    expect(text).toEqual(numberOfFiles);
+  }
+
+  async renameFile(newFileName) {
+    await this.fileTile.click({ button: "right" });
+    await this.renameFileMenuItem.click();
+    await this.fileNameInput.type(newFileName);
+    await this.page.keyboard.press("Enter");
+    const text = (await this.fileNameTitle.innerText()).valueOf();
+    expect(text).toEqual(newFileName);
+  }
+
+  async duplicateFile() {
+    await this.fileTile.click({ button: "right" });
+    await this.duplicateFileMenuItem.click();
+  }
+
+  async isSuccessMessageDisplayed(message) {
+    const text = (await this.successMessage.innerText()).valueOf();
+    expect(text).toEqual(message);
+  }
+
+  async addFileAsSharedLibrary() {
+    await this.fileTile.click({ button: "right" });
+    await this.addFileAsSharedLibraryMenuItem.click();
+    await this.addFileAsSharedLibraryButton.click();
+  }
+
+  async deleteFileAsSharedLibrary() {
+    await this.fileTile.click({ button: "right" });
+    await this.delFileAsSharedLibraryMenuItem.click();
+    await this.delFileAsSharedLibraryButton.click();
+  }
+
+  async isSharedLibraryIconDisplayed() {
+    await expect(this.sharedLibraryIcon).toBeVisible();
+  }
+  async isSharedLibraryIconNotDisplayed() {
+    await expect(this.sharedLibraryIcon).not.toBeVisible();
+  }
+
+  async downloadPenpotFile() {
+    await this.fileTile.click({ button: "right" });
+    await this.downloadFilePenpot.click();
+    await this.page.waitForEvent("download");
+    await expect(this.downloadFileTickIcon).toBeVisible();
+    await this.downloadFileCloseButton.click();
+  }
+
+  async downloadStandardFile() {
+    await this.fileTile.click({ button: "right" });
+    await this.downloadFileStandard.click();
+    await this.page.waitForEvent("download");
+    await expect(this.downloadFileTickIcon).toBeVisible();
+    await this.downloadFileCloseButton.click();
   }
 };
