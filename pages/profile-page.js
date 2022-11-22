b052da61bac2742902b7e1aa3f89927953c88385
Aliaksei Kalennikau <aliaksei.k@qawerk.com>
2022-11-22 18:58:53 +0100

    Add tests for profile

---
+++ pages/profile-page.js
@@ -0,0 +1,90 @@
+const { expect } = require("@playwright/test");
+exports.ProfilePage = class ProfilePage {
+  /**
+   * @param {import('@playwright/test').Page} page
+   */
+  constructor(page) {
+    this.page = page;
+    this.header = page.locator("h1");
+    this.profileMenuButton = page.locator('div[data-test="profile-btn"]');
+    this.yourAccountMenuItem = page.locator(
+      'li[data-test="profile-profile-opt"]'
+    );
+    this.logoutMenuItem = page.locator('li[data-test="logout-profile-opt"]');
+    this.giveFeedbackMenuItem = page.locator(
+      'li[data-test="feedback-profile-opt"]'
+    );
+    this.profileNameInput = page.locator("#fullname");
+    this.saveSettingsButton = page.locator('input[value="Save settings"]');
+    this.successMessage = page.locator('div[class="banner success fixed"]');
+    this.feedbackSubjectInput = page.locator("#subject");
+    this.feedbackDescriptionInput = page.locator("textarea");
+    this.sendFeedbackButton = page.locator('input[value="Send"]');
+  }
+
+  async openYourAccountPage() {
+    await this.profileMenuButton.click();
+    await this.yourAccountMenuItem.click();
+  }
+
+  async openGiveFeedbackPage() {
+    await this.profileMenuButton.click();
+    await this.giveFeedbackMenuItem.click();
+  }
+
+  async logout() {
+    await this.profileMenuButton.click();
+    await this.logoutMenuItem.click();
+  }
+
+  async isHeaderDisplayed(title) {
+    await expect(this.header).toBeVisible();
+    await expect(this.header).toHaveText(title);
+  }
+
+  async clearInput(input) {
+    await input.click();
+    let text = await input.inputValue();
+    for (let i = 0; i <= text.length; i++) {
+      await this.page.keyboard.press("Backspace");
+    }
+  }
+
+  async changeProfileName(newName) {
+    await this.clearInput(this.profileNameInput);
+    await this.profileNameInput.fill(newName);
+    await this.saveSettingsButton.click();
+  }
+
+  async enterSubjectToGiveFeedbackForm(text) {
+    await this.feedbackSubjectInput.fill(text);
+  }
+
+  async enterDescriptionToGiveFeedbackForm(text) {
+    await this.feedbackDescriptionInput.fill(text);
+  }
+
+  async clearSubjectInputInGiveFeedbackForm() {
+    await this.clearInput(this.feedbackSubjectInput);
+  }
+
+  async clearDescriptionInputInGiveFeedbackForm() {
+    await this.clearInput(this.feedbackDescriptionInput);
+  }
+
+  async clickSendFeedbackButton() {
+    await this.sendFeedbackButton.click();
+  }
+
+  async isSendFeedbackButtonDisabled() {
+    await expect(this.sendFeedbackButton).toBeDisabled();
+  }
+
+  async isSuccessMessageDisplayed(message) {
+    await expect(this.successMessage).toHaveText(message);
+  }
+
+  async isAccountNameDisplayed(name) {
+    await expect(this.profileMenuButton).toHaveText(name);
+  }
+};
---
+++ tests/profile.spec.js
@@ -0,0 +1,47 @@
+const { dashboardTest } = require("../fixtures");
+const { ProfilePage } = require("../pages/profile-page");
+const { random } = require("../helpers/string-generator");
+const { LoginPage } = require("../pages/login-page");
+
+dashboardTest("Edit profile name", async ({ page }) => {
+  const newName = random();
+  const profilePage = new ProfilePage(page);
+  await profilePage.openYourAccountPage();
+  await profilePage.isHeaderDisplayed("Your account");
+  await profilePage.changeProfileName(newName);
+  await profilePage.isSuccessMessageDisplayed("Profile saved successfully!");
+  await profilePage.isAccountNameDisplayed(newName);
+});
+
+dashboardTest("Logout from Account", async ({ page }) => {
+  const profilePage = new ProfilePage(page);
+  await profilePage.logout();
+  const loginPage = new LoginPage(page);
+  await loginPage.isHeaderDisplayed("Great to see you again!");
+});
+
+dashboardTest("Send feedback email with empty fields", async ({ page }) => {
+  const profilePage = new ProfilePage(page);
+  await profilePage.openGiveFeedbackPage();
+  await profilePage.isHeaderDisplayed("Your account");
+  await profilePage.isSendFeedbackButtonDisabled();
+  await profilePage.enterSubjectToGiveFeedbackForm("QA Test");
+  await profilePage.isSendFeedbackButtonDisabled();
+  await profilePage.clearSubjectInputInGiveFeedbackForm();
+  await profilePage.enterDescriptionToGiveFeedbackForm(
+    "This is a test feedback triggered by QA team"
+  );
+  await profilePage.isSendFeedbackButtonDisabled();
+});
+
+dashboardTest("Send feedback email with valid data", async ({ page }) => {
+  const profilePage = new ProfilePage(page);
+  await profilePage.openGiveFeedbackPage();
+  await profilePage.isHeaderDisplayed("Your account");
+  await profilePage.enterSubjectToGiveFeedbackForm("QA Test");
+  await profilePage.enterDescriptionToGiveFeedbackForm(
+    "This is a test feedback triggered by QA team"
+  );
+  await profilePage.clickSendFeedbackButton();
+  await profilePage.isSuccessMessageDisplayed("Feedback sent");
+});
