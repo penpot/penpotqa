003fe68a86bc94e2db6b86216fbc8f90f7d4876d
Aliaksei Kalennikau <aliaksei.k@qawerk.com>
2022-12-09 16:36:05 +0100

    Add tests for color

--- pages/color-palette-popup.js
+++ pages/color-palette-popup.js
@@ -1,4 +1,5 @@
 const { BasePage } = require("./base-page");
+const { expect } = require("@playwright/test");
 exports.ColorPalettePopUp = class ColorPalettePopUp extends BasePage {
   /**
    * @param {import('@playwright/test').Page} page
@@ -6,9 +7,22 @@ exports.ColorPalettePopUp = class ColorPalettePopUp extends BasePage {
 
   constructor(page) {
     super(page);
+    this.popUp = page.locator('div[class="colorpicker-tooltip"]');
     this.hexInput = page.locator("#hex-value");
     this.saveColorStyleButton = page.locator(
       'button:has-text("Save color style")'
+    );
+    this.firstFileLibraryColorBullet = page.locator(
+      'div[class="selected-colors"] div[class="color-bullet tooltip tooltip-right is-not-library-color"] >>nth=0'
+    );
+    this.secondFileLibraryColorBullet = page.locator(
+      'div[class="selected-colors"] div[class="color-bullet tooltip tooltip-right is-not-library-color"] >>nth=1'
+    );
+    this.recentColorsColorBullet = page.locator(
+      'div[class="selected-colors"] div[class="color-bullet tooltip tooltip-right is-library-color"] >>nth=0'
+    );
+    this.colorsSelector = page.locator(
+      'div[class="colorpicker-tooltip"] select'
     );
   }
 
@@ -19,5 +33,37 @@ exports.ColorPalettePopUp = class ColorPalettePopUp extends BasePage {
 
   async clickSaveColorStyleButton() {
     await this.saveColorStyleButton.click();
+  }
+
+  async isColorPalettePopUpOpened() {
+    await expect(this.popUp).toBeVisible();
+  }
+
+  async isFirstFileLibraryColorBulletDisplayed() {
+    await expect(this.firstFileLibraryColorBullet).toBeVisible();
+  }
+
+  async isSecondFileLibraryColorBulletDisplayed() {
+    await expect(this.secondFileLibraryColorBullet).toBeVisible();
+  }
+
+  async isRecentColorsColorBulletDisplayed() {
+    await expect(this.recentColorsColorBullet).toBeVisible();
+  }
+
+  async clickFirstFileLibraryColorBullet() {
+    await this.firstFileLibraryColorBullet.click();
+  }
+
+  async clickSecondFileLibraryColorBullet() {
+    await this.secondFileLibraryColorBullet.click();
+  }
+
+  async clickRecentColorsColorBullet() {
+    await this.recentColorsColorBullet.click();
+  }
+
+  async selectFileLibraryColors() {
+    await this.colorsSelector.selectOption("file");
   }
 };
