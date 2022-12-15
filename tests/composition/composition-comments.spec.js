90840c9fd57645fb97e9c2f54bf72c99ebed12f9
Aliaksei Kalennikau <aliaksei.k@qawerk.com>
2022-12-15 17:45:21 +0100

    composition-comments - update screenshot comparison settings

--- tests/composition/composition-comments.spec.js
+++ tests/composition/composition-comments.spec.js
@@ -2,29 +2,44 @@ const { mainTest } = require("../../fixtures");
 const { MainPage } = require("../../pages/main-page");
 const { expect } = require("@playwright/test");
 
-mainTest("CO-339 Create comment from toolbar", async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.clickCreateCommentButton();
-  await mainPage.clickViewportTwice();
-  await mainPage.enterCommentText("Test Comment");
-  await mainPage.clickPostCommentButton();
-  await mainPage.isCommentDisplayedInPopUp("Test Comment");
-  await mainPage.isCommentDisplayedInCommentsPanel("Test Comment");
-  await expect(page).toHaveScreenshot("comment-opened-pop-up.png", {
-    mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
-    maxDiffPixels: 5,
-  });
-  await mainPage.clickCreateCommentButton();
-  await mainPage.isCommentThreadIconDisplayed();
-  await expect(page).toHaveScreenshot("comment-closed-pop-up.png", {
-    mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
-    maxDiffPixels: 5,
-  });
-});
+mainTest(
+  "CO-339 Create comment from toolbar",
+  async ({ page, browserName }) => {
+    const mainPage = new MainPage(page);
+    await mainPage.clickCreateCommentButton();
+    await mainPage.clickViewportTwice();
+    await mainPage.enterCommentText("Test Comment");
+    await mainPage.clickPostCommentButton();
+    await mainPage.isCommentDisplayedInPopUp("Test Comment");
+    await mainPage.isCommentDisplayedInCommentsPanel("Test Comment");
+    if (browserName === "chromium") {
+      await expect(page).toHaveScreenshot("comment-opened-pop-up.png", {
+        mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+      });
+    } else {
+      await expect(page).toHaveScreenshot("comment-opened-pop-up.png", {
+        mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+        maxDiffPixels: 5,
+      });
+    }
+    await mainPage.clickCreateCommentButton();
+    await mainPage.isCommentThreadIconDisplayed();
+    if (browserName === "chromium") {
+      await expect(page).toHaveScreenshot("comment-closed-pop-up.png", {
+        mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+      });
+    } else {
+      await expect(page).toHaveScreenshot("comment-closed-pop-up.png", {
+        mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+        maxDiffPixels: 5,
+      });
+    }
+  }
+);
 
 mainTest(
   "CO-341 Post comment with valid text using Latin alphabet",
-  async ({ page }) => {
+  async ({ page, browserName }) => {
     const mainPage = new MainPage(page);
     await mainPage.clickCreateCommentButton();
     await mainPage.clickViewportTwice();
@@ -38,22 +53,34 @@ mainTest(
     await mainPage.isCommentDisplayedInCommentsPanel(
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
     );
-    await expect(page).toHaveScreenshot("comment-latin-opened-pop-up.png", {
-      mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
-      maxDiffPixels: 5,
-    });
+    if (browserName === "chromium") {
+      await expect(page).toHaveScreenshot("comment-latin-opened-pop-up.png", {
+        mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+      });
+    } else {
+      await expect(page).toHaveScreenshot("comment-latin-opened-pop-up.png", {
+        mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+        maxDiffPixels: 5,
+      });
+    }
     await mainPage.clickCreateCommentButton();
     await mainPage.isCommentThreadIconDisplayed();
-    await expect(page).toHaveScreenshot("comment-latin-closed-pop-up.png", {
-      mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
-      maxDiffPixels: 5,
-    });
+    if (browserName === "chromium") {
+      await expect(page).toHaveScreenshot("comment-latin-closed-pop-up.png", {
+        mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+      });
+    } else {
+      await expect(page).toHaveScreenshot("comment-latin-closed-pop-up.png", {
+        mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+        maxDiffPixels: 5,
+      });
+    }
   }
 );
 
 mainTest(
   "CO-346 Reply comment with valid text using Latin alphabet",
-  async ({ page }) => {
+  async ({ page, browserName }) => {
     const mainPage = new MainPage(page);
     await mainPage.clickCreateCommentButton();
     await mainPage.clickViewportTwice();
@@ -69,16 +96,22 @@ mainTest(
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
     );
     await mainPage.isCommentReplyDisplayedInCommentsPanel();
-    await expect(page).toHaveScreenshot("comment-reply.png", {
-      mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
-      maxDiffPixels: 5,
-    });
+    if (browserName === "chromium") {
+      await expect(page).toHaveScreenshot("comment-reply.png", {
+        mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+      });
+    } else {
+      await expect(page).toHaveScreenshot("comment-reply.png", {
+        mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+        maxDiffPixels: 5,
+      });
+    }
   }
 );
 
 mainTest(
   "CO-351 Edit comment with valid text using Latin alphabet",
-  async ({ page }) => {
+  async ({ page, browserName }) => {
     const mainPage = new MainPage(page);
     await mainPage.clickCreateCommentButton();
     await mainPage.clickViewportTwice();
@@ -97,14 +130,20 @@ mainTest(
     await mainPage.isCommentDisplayedInCommentsPanel("Edited Test Comment");
     await mainPage.clickCommentThreadIcon();
     await mainPage.isCommentDisplayedInPopUp("Edited Test Comment");
-    await expect(page).toHaveScreenshot("comment-edited.png", {
-      mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
-      maxDiffPixels: 5,
-    });
+    if (browserName === "chromium") {
+      await expect(page).toHaveScreenshot("comment-edited.png", {
+        mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+      });
+    } else {
+      await expect(page).toHaveScreenshot("comment-edited.png", {
+        mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+        maxDiffPixels: 5,
+      });
+    }
   }
 );
 
-mainTest("CO-356 Delete thread", async ({ page }) => {
+mainTest("CO-356 Delete thread", async ({ page, browserName }) => {
   const mainPage = new MainPage(page);
   await mainPage.clickCreateCommentButton();
   await mainPage.clickViewportTwice();
@@ -119,13 +158,19 @@ mainTest("CO-356 Delete thread", async ({ page }) => {
   await mainPage.isCommentsPanelPlaceholderDisplayed(
     "You have no pending comment notifications"
   );
-  await expect(page).toHaveScreenshot("comment-removed.png", {
-    mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
-    maxDiffPixels: 5,
-  });
+  if (browserName === "chromium") {
+    await expect(page).toHaveScreenshot("comment-removed.png", {
+      mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+    });
+  } else {
+    await expect(page).toHaveScreenshot("comment-removed.png", {
+      mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+      maxDiffPixels: 5,
+    });
+  }
 });
 
-mainTest("CO-360 Resolve comment", async ({ page }) => {
+mainTest("CO-360 Resolve comment", async ({ page, browserName }) => {
   const mainPage = new MainPage(page);
   await mainPage.clickCreateCommentButton();
   await mainPage.clickViewportTwice();
@@ -134,14 +179,26 @@ mainTest("CO-360 Resolve comment", async ({ page }) => {
   await mainPage.clickResolveCommentCheckbox();
   await mainPage.clickCreateCommentButton();
   await mainPage.isCommentResolvedThreadIconDisplayed();
-  await expect(page).toHaveScreenshot("comment-resolved-closed-pop-up.png", {
-    mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
-    maxDiffPixels: 5,
-  });
+  if (browserName === "chromium") {
+    await expect(page).toHaveScreenshot("comment-resolved-closed-pop-up.png", {
+      mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+    });
+  } else {
+    await expect(page).toHaveScreenshot("comment-resolved-closed-pop-up.png", {
+      mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+      maxDiffPixels: 5,
+    });
+  }
   await mainPage.clickResolvedCommentThreadIcon();
   await mainPage.isResolveCommentCheckboxSelected();
-  await expect(page).toHaveScreenshot("comment-resolved-opened-pop-up.png", {
-    mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
-    maxDiffPixels: 5,
-  });
+  if (browserName === "chromium") {
+    await expect(page).toHaveScreenshot("comment-resolved-opened-pop-up.png", {
+      mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+    });
+  } else {
+    await expect(page).toHaveScreenshot("comment-resolved-opened-pop-up.png", {
+      mask: [mainPage.commentsAuthorSection, mainPage.usersSection],
+      maxDiffPixels: 5,
+    });
+  }
 });
