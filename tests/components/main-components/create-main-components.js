b4e9420eff1a8a3ffc02f798be48502a55cfa6c5
KateMaruk <marukkatia@gmail.com>
2023-11-25 22:57:53 +0200

    added tests for main components (creating)

---
+++ tests/components/main-components/create-main-components.js
@@ -0,0 +1,8 @@
+const { mainTest } = require("../../");
+const { MainPage } = require("../../pages/main-page");
+const { expect } = require("@playwright/test");
+
+mainTest('', async () => {
+
+});
+
