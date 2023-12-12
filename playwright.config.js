27926d1434fd1f4d81f29e1b554d24b80d4c8c5d
KateMaruk <marukkatia@gmail.com>
2023-12-12 09:37:35 +0200

    added tests for components v2 - delete

--- playwright.config.js
+++ playwright.config.js
@@ -25,9 +25,9 @@ const config = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 2,
   /* Opt out of parallel tests on CI. */
-  workers: 1,
+  workers: process.env.CI ? 2 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
