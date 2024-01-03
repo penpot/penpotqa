00e8f25fe986e2a2e7ae996e9abeec52df4d2177
chalapkoStanislav <154974553+chalapkoStanislav@users.noreply.github.com>
2024-01-03 17:48:03 +0200

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -36,7 +36,7 @@ const config = {
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASE_URL,
-    headless: false,
+    headless: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     video: 'on',
