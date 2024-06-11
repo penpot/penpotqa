52751a516f2efb23cda78211a735d4d03a90982e
Staschalapko <cstanislav@qawerk.com>
2024-06-11 18:18:26 +0300

    test fix

--- pages/workspace/main-page.js
+++ pages/workspace/main-page.js
@@ -899,7 +899,7 @@ exports.MainPage = class MainPage extends BasePage {
   }
 
   async duplicateLayerViaRightClick() {
-    const layerSel = this.page.locator('div[class="viewport"] [id^="shape"]');
+    const layerSel = this.page.locator('.viewport-selrect');
     await layerSel.last().click({ button: 'right', force: true });
     await this.duplicateOption.click();
   }
