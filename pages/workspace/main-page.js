f20fe55ec0b27f659b6c9f9a8d57bb8781d8cbd3
chalapkoStanislav <cstanislav@redwerk.com>
2024-07-14 19:03:50 +0300

    update snapshots

--- pages/workspace/main-page.js
+++ pages/workspace/main-page.js
@@ -841,11 +841,14 @@ exports.MainPage = class MainPage extends BasePage {
     await this.waitForChangeIsSaved();
   }
 
-  async createDefaultTextLayer() {
+  async createDefaultTextLayer(browserName) {
     await this.clickCreateTextButton();
     await this.clickViewportByCoordinates(200, 300);
     const platform = getPlatformName()
     if (platform === 'darwin') {
+      await this.typeTextFromKeyboard();
+    } else if(browserName === 'webkit') {
+      await this.page.waitForTimeout(400);
       await this.typeTextFromKeyboard();
     } else {
       await this.typeText('Hello World!');
