0bda0002785c70ae196a291fd7a2d57d6ea8e4eb
Staschalapko <cstanislav@qawerk.com>
2024-07-20 18:23:14 +0300

    685,688,689,690,691,698,699,700 tests added

--- pages/workspace/view-mode-page.js
+++ pages/workspace/view-mode-page.js
@@ -13,6 +13,28 @@ exports.ViewModePage = class ViewModePage extends BasePage {
     this.viewerLoyautSection = page.locator('#viewer-layout');
     this.inspectButton = page.locator(`button .icon-code`);
     this.rightSidebar = page.locator(`aside[class*='inspect_right_sidebar']`);
+    this.fullScreenButton = page.locator(`span[class*="fullscreen-btn"]`);
+    this.fullScreenSection = page.locator(`section[class*="main_ui_viewer__fulscreen"]`);
+    this.prevBoardButton = page.locator(`button[class*="viewer-go-prev"]`);
+    this.nextBoardButton = page.locator(`button[class*="viewer-go-next"]`);
+    this.resetBoardButton = page.locator(`button[class*="reset-button"]`);
+    this.selectBoardDropdown = page.locator('#current-frame');
+    this.boardTitle = page.locator('div[class*="thumbnail-info"]');
+    this.interactionsArrow = page.locator('.interactions .selected');
+    this.interactionsDropdown = page.locator('span[class*="interactions__dropdown-title"]:has-text("Interactions")');
+    this.interactionsDropdownOptions = page.locator('ul[class*="interactions__dropdown"]');
+    this.dontShowInterOptions = page.locator('li[data-mode="hide"]');
+    this.showInterOptions = page.locator('li[data-mode="show"]');
+    this.showOnClickInterOptions = page.locator('li[data-mode="show-on-click"]');
+    this.boardCounter = page.locator('.main_ui_viewer__counter');
+    this.scaleDropdown = page.locator('div[class*="header__zoom-widget"]');
+    this.upscaleButton = page.locator('button[class*="zoom-btn"] .icon-remove');
+    this.downscaleButton = page.locator('button[class*="zoom-btn"] .icon-add');
+    this.scaleResetButton = page.locator('button[class*="reset-btn"]');
+    this.scaleFitOptions = page.locator('li[class*="zoom-option"]:has-text("Fit - Scale down to fit")');
+    this.scaleFillOptions = page.locator('li[class*="zoom-option"]:has-text("Fill - Scale to fill")');
+    this.scaleFullScreenOptions = page.locator('li[class*="zoom-option"]:has-text("Full screen")');
+    this.scaleDropdownOptions = page.locator('ul[class*="header__dropdown"]');
   }
 
   async clickViewModeButton() {
@@ -21,8 +43,90 @@ exports.ViewModePage = class ViewModePage extends BasePage {
     return popupPromise;
   }
 
+  async clickViewModeShortcut() {
+    const popupPromise = this.page.waitForEvent('popup');
+    await this.page.keyboard.press('G+V');
+    return popupPromise;
+  }
+
   async openInspectTab() {
     await this.inspectButton.click();
     await expect(this.rightSidebar).toBeVisible();
+  }
+
+  async clickFullScreenButton() {
+    await this.fullScreenButton.click();
+    await expect(this.fullScreenSection).toBeVisible();
+  }
+
+  async clickPrevButton() {
+    await this.prevBoardButton.click();
+  }
+
+  async clickNextButton() {
+    await this.nextBoardButton.click();
+  }
+
+  async clickResetButton() {
+    await this.resetBoardButton.click();
+  }
+
+  async clickSelectBoardDropdown() {
+    await this.selectBoardDropdown.click();
+  }
+
+  async selectFirstBoard() {
+    await this.boardTitle.first().click();
+  }
+
+  async selectSecondBoard() {
+    await this.boardTitle.last().click();
+  }
+
+  async clickInteractionsDropdown() {
+    await this.interactionsDropdown.click();
+  }
+
+  async selectShowInteractionsOptions() {
+    await this.showInterOptions.click();
+  }
+
+  async selectShowOnClickInteractionsOptions() {
+    await this.showOnClickInterOptions.click();
+  }
+
+  async clickOnViewSection() {
+    await this.viewerLoyautSection.click();
+  }
+
+  async clickOnBoardCounter() {
+    await this.boardCounter.click();
+  }
+
+  async openScaleDropdown() {
+    await this.scaleDropdown.click();
+  }
+  async clickDownscaleButton() {
+    await this.downscaleButton.click();
+  }
+
+  async clickUpscaleButton() {
+    await this.upscaleButton.click();
+  }
+
+  async clickResetScaleButton() {
+    await this.scaleResetButton.click();
+  }
+
+  async selectFitScaleOptions() {
+    await this.scaleFitOptions.click();
+  }
+
+  async selectFillScaleOptions() {
+    await this.scaleFillOptions.click();
+  }
+
+  async selectFullScreenScaleOptions() {
+    await this.scaleFullScreenOptions.click();
   }
 };
---
+++ tests/view-mode.spec.js
@@ -0,0 +1,207 @@
+const { expect, test } = require('@playwright/test');
+const { mainTest } = require('../fixtures');
+const { MainPage } = require('../pages/workspace/main-page');
+const { random } = require('../helpers/string-generator');
+const { TeamPage } = require('../pages/dashboard/team-page');
+const { DashboardPage } = require('../pages/dashboard/dashboard-page');
+const { updateTestResults } = require('./../helpers/saveTestResults.js');
+const { qase } = require('playwright-qase-reporter/dist/playwright');
+const { ViewModePage } = require('../pages/workspace/view-mode-page');
+const { PrototypePanelPage } = require('../pages/workspace/prototype-panel-page');
+
+const teamName = random().concat('autotest');
+
+let teamPage,dashboardPage,mainPage,viewModePage,prototypePanelPage;
+test.beforeEach(async ({ page }) => {
+  teamPage = new TeamPage(page);
+  dashboardPage = new DashboardPage(page);
+  mainPage = new MainPage(page);
+  viewModePage = new ViewModePage(page);
+  prototypePanelPage = new PrototypePanelPage(page);
+  await teamPage.createTeam(teamName);
+  await teamPage.isTeamSelected(teamName);
+  await dashboardPage.createFileViaPlaceholder();
+  await mainPage.isMainPageLoaded();
+});
+
+test.afterEach(async ({ page }, testInfo) => {
+  const teamPage = new TeamPage(page);
+  const mainPage = new MainPage(page);
+  await mainPage.backToDashboardFromFileEditor();
+  await teamPage.deleteTeam(teamName);
+  await updateTestResults(testInfo.status, testInfo.retry)
+});
+
+mainTest(qase([685],'CO-364 Click view mode (From right top click) - no boards created'), async ({ page }) => {
+  const newPage = await viewModePage.clickViewModeButton();
+  viewModePage = new ViewModePage(newPage);
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'no-board-view-mode-page-image.png'
+  );
+});
+
+mainTest(qase([688],'CO-367 Click view mode (From shortcut G+V) - board is created'), async ({ page }) => {
+  await mainPage.createDefaultBoardByCoordinates(300, 300);
+  await mainPage.waitForChangeIsSaved();
+  const newPage = await viewModePage.clickViewModeShortcut();
+  viewModePage = new ViewModePage(newPage);
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'view-mode-page-image.png'
+  );
+});
+
+mainTest(qase([690],'CO-369 Full screen on/off'), async ({ page }) => {
+  await mainPage.createDefaultBoardByCoordinates(300, 300);
+  await mainPage.waitForChangeIsSaved();
+  const newPage = await viewModePage.clickViewModeShortcut();
+  viewModePage = new ViewModePage(newPage);
+  await viewModePage.clickFullScreenButton();
+  await expect(viewModePage.fullScreenSection).toHaveScreenshot(
+    'view-mode-full-screen-image.png'
+  );
+  await viewModePage.clickOnESC();
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'view-mode-page-image.png'
+  );
+});
+
+mainTest(qase([698],'CO-377 Click arrows to navigate to other boards'), async ({ page }) => {
+  await mainPage.createDefaultBoardByCoordinates(300, 300);
+  await mainPage.waitForChangeIsSaved();
+  await mainPage.createDefaultBoardByCoordinates(500, 500, true);
+  await mainPage.waitForChangeIsSaved();
+  const newPage = await viewModePage.clickViewModeShortcut();
+  viewModePage = new ViewModePage(newPage);
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'first-board-view-mode-page-image.png'
+  );
+  await viewModePage.clickNextButton();
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'second-board-view-mode-page-image.png'
+  );
+  await viewModePage.clickPrevButton();
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'first-board-view-mode-page-image.png'
+  );
+  await viewModePage.clickNextButton();
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'second-board-view-mode-page-image.png'
+  );
+  await viewModePage.clickPrevButton();
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'first-board-view-mode-page-image.png'
+  );
+});
+
+mainTest(qase([700],'CO-379 Click Back icon to reset view'), async ({ page }) => {
+  await mainPage.createDefaultBoardByCoordinates(300, 300);
+  await mainPage.waitForChangeIsSaved();
+  await mainPage.createDefaultBoardByCoordinates(500, 500, true);
+  await mainPage.waitForChangeIsSaved();
+  await mainPage.createDefaultBoardByCoordinates(100, 100, true);
+  await mainPage.waitForChangeIsSaved();
+  const newPage = await viewModePage.clickViewModeShortcut();
+  viewModePage = new ViewModePage(newPage);
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'first-board-view-mode-page-image.png'
+  );
+  await viewModePage.clickNextButton();
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'second-board-view-mode-page-image.png'
+  );
+  await viewModePage.clickNextButton();
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'third-board-view-mode-page-image.png'
+  );
+  await viewModePage.clickResetButton();
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'first-board-view-mode-page-image.png'
+  );
+});
+
+mainTest(qase([699],'CO-378 Click board dropdown to navigate to other boards'), async ({ page }) => {
+  await mainPage.createDefaultBoardByCoordinates(300, 300);
+  await mainPage.waitForChangeIsSaved();
+  await mainPage.createDefaultBoardByCoordinates(500, 500, true);
+  await mainPage.waitForChangeIsSaved();
+  const newPage = await viewModePage.clickViewModeShortcut();
+  viewModePage = new ViewModePage(newPage);
+  await viewModePage.clickSelectBoardDropdown();
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'board-dropdown-view-mode-page-image.png'
+  );
+  await viewModePage.selectSecondBoard();
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'second-board-selected-view-mode-page-image.png'
+  );
+  await viewModePage.selectFirstBoard();
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'first-board-selected-view-mode-page-image.png'
+  );
+});
+
+// mainTest(qase([689],'CO-368 Interactions dropdown'), async ({ page }) => {
+//   await mainPage.createDefaultBoardByCoordinates(300, 300);
+//   await mainPage.createDefaultBoardByCoordinates(500, 500, true);
+//   await mainPage.waitForChangeIsSaved();
+//   await prototypePanelPage.clickPrototypeTab();
+//   await prototypePanelPage.dragAndDropPrototypeArrowConnector(300, 300);
+//   const newPage = await viewModePage.clickViewModeShortcut();
+//   viewModePage = new ViewModePage(newPage);
+//   await viewModePage.clickInteractionsDropdown();
+//   await expect(viewModePage.interactionsDropdownOptions).toHaveScreenshot(
+//     'interactions-dont-show-options-image.png'
+//   );
+//   await viewModePage.selectShowInteractionsOptions();
+//   await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+//     'show-interactions-board-view-mode-page-image.png'
+//   );
+//   // await viewModePage.clickInteractionsDropdown();
+//   await expect(viewModePage.interactionsDropdownOptions).toHaveScreenshot(
+//     'interactions-show-options-image.png'
+//   );
+//   await viewModePage.selectShowOnClickInteractionsOptions();
+//   await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+//     'show-on-click-interactions-board-view-mode-page-image.png'
+//   );
+//   await viewModePage.clickOnBoardCounter();
+//   await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+//     'show-on-click-interactions-board-view-mode-page-image2.png'
+//   );
+// });
+
+mainTest(qase([691],'CO-370 Change scale'), async ({ page }) => {
+  await mainPage.createDefaultBoardByCoordinates(300, 300);
+  await mainPage.waitForChangeIsSaved();
+  const newPage = await viewModePage.clickViewModeShortcut();
+  viewModePage = new ViewModePage(newPage);
+  await viewModePage.openScaleDropdown();
+  await expect(viewModePage.scaleDropdownOptions).toHaveScreenshot(
+    'scale-dropdown-view-mode-page-image.png'
+  );
+  await viewModePage.clickDownscaleButton();
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'downscale-board-view-mode-page-image.png'
+  );
+  await viewModePage.clickResetScaleButton();
+  await viewModePage.clickUpscaleButton();
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'upscale-board-view-mode-page-image.png'
+  );
+  await viewModePage.selectFitScaleOptions();
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'fit-scale-board-view-mode-page-image.png'
+  );
+  await viewModePage.selectFillScaleOptions();
+  await expect(viewModePage.viewerLoyautSection).toHaveScreenshot(
+    'fill-scale-board-view-mode-page-image.png'
+  );
+  await viewModePage.selectFullScreenScaleOptions();
+  await expect(viewModePage.fullScreenSection).toHaveScreenshot(
+    'full-screen-scale-board-view-mode-page-image.png'
+  );
+  await viewModePage.clickResetScaleButton();
+  await expect(viewModePage.fullScreenSection).toHaveScreenshot(
+    'full-screen-default-scale-board-view-mode-page-image.png'
+  );
+});
