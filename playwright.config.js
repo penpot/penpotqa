5b8e608eb2adb8935d5f5e07d9496cb799877070
stanislavchalapko <staschalapko@gmail.com>
2024-07-29 14:29:44 +0300

    test

--- playwright.config.js
+++ playwright.config.js
@@ -12,7 +12,7 @@ const config = {
   snapshotPathTemplate: `{testDir}/{testFileDir}/{testFileName}-snapshots/win32/{projectName}/{arg}{ext}`,
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: process.env.CI ? 120 * 1000 : 80 * 1000,
+  timeout: process.env.CI ? 110 * 1000 : 80 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
