0226a167a7f060e5916bbe2ce855f5219e1cb40f
chalapkoStanislav <cstanislav@redwerk.com>
2024-08-26 22:25:44 +0300

    fix

--- tests/composition/composition-text.spec.js
+++ tests/composition/composition-text.spec.js
@@ -106,7 +106,7 @@ mainTest.describe(() => {
     },
   );
 
-  mainTest(qase(382,'CO-167 Add and edit Shadow to text'), async ({ page }) => {
+  mainTest(qase(382,'CO-167 Add and edit Shadow to text'), async ({ page, browserName }) => {
     const mainPage = new MainPage(page);
     const colorPalettePage = new ColorPalettePage(page);
     const designPanelPage = new DesignPanelPage(page);
@@ -116,6 +116,7 @@ mainTest.describe(() => {
     await designPanelPage.clickShadowColorIcon();
     await colorPalettePage.setHex('#304d6a');
     await mainPage.clickMoveButton();
+    browserName === 'webkit' ? await mainPage.waitForChangeIsUnsaved() : null;
     await mainPage.waitForChangeIsSaved();
     await expect(mainPage.viewport).toHaveScreenshot('text-drop-shadow.png');
     await designPanelPage.selectTypeForShadow('Inner shadow');
