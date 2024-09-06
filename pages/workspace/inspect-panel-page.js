5ee22c3e6ec869cfe53f3e290a38b9b449004c67
chalapkoStanislav <cstanislav@qawerk.com>
2024-09-06 15:55:25 +0300

    getByRole locators

--- pages/workspace/inspect-panel-page.js
+++ pages/workspace/inspect-panel-page.js
@@ -18,7 +18,7 @@ exports.InspectPanelPage = class InspectPanelPage extends BasePage {
     this.rowGapOnInspect = page.locator(
       'div[class*="layout-row"] div[title="Row gap"]',
     );
-    this.codeTabButton = page.locator('button[data-id="code"]');
+    this.codeTabButton = page.getByRole("tab", { name: "code" });
   }
 
   async openInspectTab() {
