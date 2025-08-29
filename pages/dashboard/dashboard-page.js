f1523262828d4bb8892a9dc4cffb0c40a6307360
lugarbos <lucia.garcia@kaleidos.net>
2025-08-29 11:13:01 +0200

    refactor(pom): changed name by projectName in all methods using name of a project

--- pages/dashboard/dashboard-page.js
+++ pages/dashboard/dashboard-page.js
@@ -519,8 +519,8 @@ exports.DashboardPage = class DashboardPage extends BasePage {
     await expect(this.header).toHaveText(item);
   }
 
-  async getProjectHeadingByName(name) {
-    return this.projectNameTitle.getByText(name, { exact: true });
+  async getProjectHeadingByName(projectName) {
+    return this.projectNameTitle.getByText(projectName, { exact: true });
   }
 
   /**
@@ -543,14 +543,14 @@ exports.DashboardPage = class DashboardPage extends BasePage {
    * @param {string} name - The name of the project.
    * @returns {Locator} - The locator for the project wrapper element.
    */
-  async getProjectWrapperByName(name) {
+  async getProjectWrapperByName(projectName) {
     return this.page
       .locator('.main_ui_dashboard_projects__project-name-wrapper')
-      .filter({ hasText: name }, { exact: true });
+      .filter({ hasText: projectName }, { exact: true });
   }
 
-  async getPinnedProjectItemFromSidebar(name) {
-    return this.pinnedProjectsSidebarItem.getByText(name);
+  async getPinnedProjectItemFromSidebar(projectName) {
+    return this.pinnedProjectsSidebarItem.getByText(projectName);
   }
 
   async openPinnedProjectFromSidebar(projectName) {
@@ -618,8 +618,8 @@ exports.DashboardPage = class DashboardPage extends BasePage {
    * Check if the pinned project item name is truncated with '...' in the sidebar.
    * @param name of the pinned project item
    */
-  async isPinnedProjectItemNameTruncated(name) {
-    const pinnedItem = await this.getPinnedProjectItemFromSidebar(name);
+  async isPinnedProjectItemNameTruncated(projectName) {
+    const pinnedItem = await this.getPinnedProjectItemFromSidebar(projectName);
     const elementHandle = await pinnedItem.elementHandle();
 
     const isTruncated = await elementHandle.evaluate((el) => {
