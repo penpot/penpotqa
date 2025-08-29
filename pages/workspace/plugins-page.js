fc2bce8b9818910fe3c8443c3a0bef52c9126821
lugarbos <lucia.garcia@kaleidos.net>
2025-08-29 10:43:33 +0200

    refactor(page/tests): refactor plugins page object and tests to remove snapshots

--- pages/workspace/plugins-page.js
+++ pages/workspace/plugins-page.js
@@ -13,8 +13,10 @@ exports.PluginsPage = class PluginsPage extends MainPage {
 
     // Main Toolbar
     this.pluginsButton = page.getByRole('button', { name: /plugins/i });
+
     // Main Menu - first level
     this.pluginsMainMenuItem = page.getByRole('menuitem').getByText('Plugins');
+
     // Main Menu - second level
     this.pluginsManagerButton = page
       .getByRole('menuitem')
@@ -24,38 +26,51 @@ exports.PluginsPage = class PluginsPage extends MainPage {
       .getByText('Lorem ipsum');
 
     // Plugin Manager panel
-    this.pluginPanel = page.locator(
-      'div[class$="workspace_plugins__plugin-management"]',
+    this.pluginManagementModal = page.locator(
+      '.main_ui_workspace_plugins__plugin-management',
     );
-    this.closePluginPanelButton = page.locator(
-      'button[class$="plugins__close-btn"]',
+    this.closePluginPanelButton = this.pluginManagementModal.locator(
+      '.main_ui_workspace_plugins__close-btn',
     );
-    this.urlPluginPanelInput = page
-      .getByRole('textbox')
-      .and(page.getByPlaceholder('Write a plugin URL'));
-    this.installPluginPanelButton = page
+    this.urlPluginPanelInput =
+      this.pluginManagementModal.getByPlaceholder('Write a plugin URL');
+    this.installPluginPanelButton = this.pluginManagementModal
       .getByRole('button', { name: 'Install' })
       .first();
-    this.deletePluginPanelButton = page
+    this.deletePluginPanelButton = this.pluginManagementModal
       .getByRole('button', { name: 'Remove plugin' })
       .first();
-    this.allowPluginPanelButton = page.getByRole('button', { name: 'Allow' });
-    this.openPluginPanelButton = page.locator('button', { name: 'Open' });
-    this.installedPluginPanelListElements = page.locator(
-      'div[class$="workspace_plugins__plugins-list-element"]',
+    this.pluginList = this.pluginManagementModal.locator(
+      '.main_ui_workspace_plugins__plugins-list',
+    );
+    this.pluginItem = this.pluginManagementModal.locator(
+      '.main_ui_workspace_plugins__plugins-list-element',
+    );
+    this.noPluginsText = this.pluginManagementModal.getByText(
+      'No plugins installed yet',
     );
 
-    // Lorem Ipsum panel
+    // Permissions Modal
+    this.allowPluginPanelButton = this.page.getByRole('button', { name: 'Allow' });
+    this.openPluginPanelButton = this.pluginManagementModal.getByRole('button', {
+      name: 'Open',
+    });
+
+    // Lorem Ipsum Plugin Panel
     this.loremPluginPanel = page
       .getByTitle('LOREM IPSUM PLUGIN')
       .locator('div')
       .first();
-    this.loremIpsumPluginGenerateButton = page
-      .frameLocator('[src*="https://lorem-ipsum-penpot-plugin.pages.dev"]')
-      .getByRole('button', { name: /Generate/i });
+    this.loremIpsumFrame = page.frameLocator(
+      '[src*="https://lorem-ipsum-penpot-plugin.pages.dev"]',
+    );
+    this.loremIpsumPluginGenerationOptions =
+      this.loremIpsumFrame.locator('.generation-options');
+    this.loremIpsumPluginGenerateButton = this.loremIpsumFrame.getByRole('button', {
+      name: /Generate/i,
+    });
   }
 
-  // POM Methods
   async clickPluginsButton() {
     await this.pluginsButton.click();
   }
@@ -91,18 +106,52 @@ exports.PluginsPage = class PluginsPage extends MainPage {
 
   async clickClosePluginPanelButton() {
     await this.closePluginPanelButton.click();
-    await expect(this.pluginPanel).not.toBeVisible();
   }
 
-  async checkInstalledPluginsCountIs(count) {
-    expect(await this.installedPluginPanelListElements.count()).toBe(count);
+  async isInstalledPluginsCount(count) {
+    await expect(this.pluginItem, `Plugin count is ${count}`).toHaveCount(count);
   }
 
   async setPluginLoremIpsumUrl() {
     await this.setPluginUrl(LOREM_IPSUM_PLUGIN_URL);
   }
 
-  async checkLoremIpsumPluginIsVisible() {
-    await expect(this.loremIpsumPluginGenerateButton).toBeVisible();
+  async isLoremIpsumPluginVisible() {
+    await expect(
+      this.loremIpsumPluginGenerationOptions,
+      'Plugin generation options is visible',
+    ).toBeVisible();
+    await expect(
+      this.loremIpsumPluginGenerateButton,
+      'Generate button is visible',
+    ).toBeVisible();
+  }
+
+  async isPluginManagerModalVisible() {
+    await expect(
+      this.pluginManagementModal,
+      'Plugin Manager modal is visible',
+    ).toBeVisible();
+  }
+
+  async isPluginManagerModalNotVisible() {
+    await expect(
+      this.pluginManagementModal,
+      'Plugin Manager modal is not visible',
+    ).not.toBeVisible();
+  }
+
+  async isOpenPluginButtonVisible() {
+    await expect(
+      this.openPluginPanelButton,
+      'Open plugin button is visible',
+    ).toBeVisible();
+  }
+
+  async isNoPluginMessageVisible() {
+    await expect(
+      this.noPluginsText,
+      `"No plugins installed yet" message is visible`,
+    ).toBeVisible();
   }
 };
