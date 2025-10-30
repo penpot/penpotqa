104a6828b62c281620ce2826b9208ffa16bc6fd0
lugarbos <lucia.garcia@kaleidos.net>
2025-10-30 14:09:09 +0100

    feat(teardown): adds tags for perf, grep invert in config and modify workflows

--- tests/performance/measure.spec.js
+++ tests/performance/measure.spec.js
@@ -5,43 +5,61 @@ function waitForSeconds(timeInSeconds) {
   return new Promise((resolve) => setTimeout(() => resolve(), timeInSeconds * 1000));
 }
 
-test('PERF Measure frames per second', async ({ page }) => {
-  const performancePage = new PerformancePage(page);
-  await performancePage.injectFrameRateRecorder();
-  await performancePage.startRecordingFrameRate();
-  await waitForSeconds(3);
-  const records = await performancePage.stopRecordingFrameRate();
-  // This is an example of how to use the performance page
-  // API, in idle time we should have average ~60 frames per second.
-  const averageFrameRate =
-    records.reduce((acc, record) => acc + record.value, 0) / records.length;
-  expect(averageFrameRate).toBeGreaterThan(55);
-  expect(averageFrameRate).toBeLessThan(65);
-});
+test(
+  'PERF Measure frames per second',
+  {
+    tag: '@perf',
+  },
+  async ({ page }) => {
+    const performancePage = new PerformancePage(page);
+    await performancePage.injectFrameRateRecorder();
+    await performancePage.startRecordingFrameRate();
+    await waitForSeconds(3);
+    const records = await performancePage.stopRecordingFrameRate();
+    // This is an example of how to use the performance page
+    // API, in idle time we should have average ~60 frames per second.
+    const averageFrameRate =
+      records.reduce((acc, record) => acc + record.value, 0) / records.length;
+    expect(averageFrameRate).toBeGreaterThan(55);
+    expect(averageFrameRate).toBeLessThan(65);
+  },
+);
 
-test('PERF Measure lockings (frames per second ~= 0)', async ({ page }) => {
-  const performancePage = new PerformancePage(page);
-  await performancePage.injectFrameRateRecorder();
-  await performancePage.startRecordingFrameRate();
-  // Do something intensive that "locks" the main thread.
-  await page.evaluate(() => {
-    const start = performance.now();
-    while (performance.now() - start < 3000) {
-      // Do nothing. Just locks the main thread
-    }
-  });
-  const records = await performancePage.stopRecordingFrameRate();
-  // If frame rate is less than 5 frames per second we can assume
-  // that the main thread was locked.
-  records.some((record) => expect(record.value).toBeLessThan(5));
-});
+test(
+  'PERF Measure lockings (frames per second ~= 0)',
+  {
+    tag: '@perf',
+  },
+  async ({ page }) => {
+    const performancePage = new PerformancePage(page);
+    await performancePage.injectFrameRateRecorder();
+    await performancePage.startRecordingFrameRate();
+    // Do something intensive that "locks" the main thread.
+    await page.evaluate(() => {
+      const start = performance.now();
+      while (performance.now() - start < 3000) {
+        // Do nothing. Just locks the main thread
+      }
+    });
+    const records = await performancePage.stopRecordingFrameRate();
+    // If frame rate is less than 5 frames per second we can assume
+    // that the main thread was locked.
+    records.some((record) => expect(record.value).toBeLessThan(5));
+  },
+);
 
-test('PERF Measure long tasks', async ({ page }) => {
-  const performancePage = new PerformancePage(page);
-  await performancePage.startObservingLongTasks();
-  await waitForSeconds(3);
-  const records = await performancePage.stopObservingLongTasks();
-  // This is an example of how to use the performance page
-  // API, in idle time we should have no long tasks.
-  expect(records.length).toBe(0);
-});
+test(
+  'PERF Measure long tasks',
+  {
+    tag: '@perf',
+  },
+  async ({ page }) => {
+    const performancePage = new PerformancePage(page);
+    await performancePage.startObservingLongTasks();
+    await waitForSeconds(3);
+    const records = await performancePage.stopObservingLongTasks();
+    // This is an example of how to use the performance page
+    // API, in idle time we should have no long tasks.
+    expect(records.length).toBe(0);
+  },
+);
--- tests/performance/thumbnail.spec.js
+++ tests/performance/thumbnail.spec.js
@@ -2,27 +2,37 @@ import { expect } from '@playwright/test';
 import { performanceTest } from '../../fixtures.js';
 import { PerformancePage } from '../../pages/performance-page';
 
-performanceTest('PERF Thumbnail renderer', async ({ page }) => {
-  const performancePage = new PerformancePage(page);
-  await performancePage.setup();
+performanceTest(
+  'PERF Thumbnail renderer',
+  {
+    tag: '@perf',
+  },
+  async ({ page }) => {
+    const performancePage = new PerformancePage(page);
+    await performancePage.setup();
 
-  // TODO: Medir el tiempo que tarda en renderizar el thumbnail.
+    // TODO: Medir el tiempo que tarda en renderizar el thumbnail.
 
-  const content = page.locator('.frame-container > .frame-content');
-  await content.waitFor();
-  performance.mark('imposter:start');
+    const content = page.locator('.frame-container > .frame-content');
+    await content.waitFor();
+    performance.mark('imposter:start');
 
-  const imposter = page.locator('.frame-container > .frame-imposter');
-  await imposter.waitFor();
-  performance.mark('imposter:end');
+    const imposter = page.locator('.frame-container > .frame-imposter');
+    await imposter.waitFor();
+    performance.mark('imposter:end');
 
-  const measure = performance.measure('imposter', 'imposter:start', 'imposter:end');
-  console.log(measure.duration, measure.entryType, measure.name);
+    const measure = performance.measure(
+      'imposter',
+      'imposter:start',
+      'imposter:end',
+    );
+    console.log(measure.duration, measure.entryType, measure.name);
 
-  const [averageFrameRate, averageLongTaskDuration] =
-    await performancePage.measure();
+    const [averageFrameRate, averageLongTaskDuration] =
+      await performancePage.measure();
 
-  expect(averageFrameRate).toBeGreaterThan(55);
-  expect(averageFrameRate).toBeLessThan(65);
-  expect(averageLongTaskDuration).toBeLessThan(0.5);
-});
+    expect(averageFrameRate).toBeGreaterThan(55);
+    expect(averageFrameRate).toBeLessThan(65);
+    expect(averageLongTaskDuration).toBeLessThan(0.5);
+  },
+);
