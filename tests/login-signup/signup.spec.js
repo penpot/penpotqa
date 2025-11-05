54b44518a3f801deef3ba097124ba3acda17da4a
lugarbos <lucia.garcia@kaleidos.net>
2025-11-05 12:59:49 +0100

    refactor(registerPage): refactor register pom and tests

--- tests/login-signup/signup.spec.js
+++ tests/login-signup/signup.spec.js
@@ -10,7 +10,7 @@ const { TeamPage } = require('../../pages/dashboard/team-page.js');
 
 let loginPage, registerPage, dashboardPage, teamPage;
 
-test.describe(() => {
+test.describe('Sign up negative cases', () => {
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
     registerPage = new RegisterPage(page);
@@ -22,40 +22,39 @@ test.describe(() => {
     await loginPage.clickOnCreateAccount();
   });
 
-  test(qase(32, 'ON-5 Sign up with invalid email address'), async () => {
+  test(qase(32, 'Sign up with invalid email address'), async () => {
     await registerPage.isRegisterPageOpened();
     await registerPage.enterEmail('test.com');
     await registerPage.enterPassword(process.env.LOGIN_PWD);
     await registerPage.isEmailInputErrorDisplayed('Enter a valid email please');
-    await registerPage.isCreateAccountBtnDisplayed();
-    await registerPage.isCreateAccountBtnDisabled();
+    await registerPage.isCreateAccountButtonVisible();
+    await registerPage.isCreateAccountButtonDisabled();
   });
 
-  test(qase(33, 'ON-6 Sign up with no password'), async () => {
+  test(qase(33, 'Sign up with no password'), async () => {
     await registerPage.isRegisterPageOpened();
     await registerPage.enterEmail(process.env.LOGIN_EMAIL);
     await registerPage.clickOnPasswordInput();
     await registerPage.clickOnHeader();
-    await registerPage.isPasswordInputHintDisplayed('At least 8 characters');
-    await registerPage.isCreateAccountBtnDisplayed();
-    await registerPage.isCreateAccountBtnDisabled();
+    await registerPage.isPasswordInputHintVisible();
+    await registerPage.isCreateAccountButtonVisible();
+    await registerPage.isCreateAccountButtonDisabled();
   });
 
-  test(qase(34, 'ON-7 Sign up with incorrect password'), async () => {
+  test(qase(34, 'Sign up with incorrect password'), async () => {
     await registerPage.isRegisterPageOpened();
     await registerPage.enterEmail(process.env.LOGIN_EMAIL);
     await registerPage.enterPassword('1234');
     await registerPage.isPasswordInputErrorDisplayed(
       'Password should at least be 8 characters',
     );
-    await registerPage.isCreateAccountBtnDisplayed();
-    await registerPage.isCreateAccountBtnDisabled();
+    await registerPage.isCreateAccountButtonVisible();
+    await registerPage.isCreateAccountButtonDisabled();
   });
 
-  test.describe(() => {
+  test.describe('Sign up via email invitation', () => {
     let randomName, email, invite;
     test.beforeEach(async ({ page }) => {
-      await test.slow();
       randomName = random().concat('autotest');
       email = `${process.env.GMAIL_NAME}+${randomName}${process.env.GMAIL_DOMAIN}`;
       await registerPage.registerAccount(randomName, email, process.env.LOGIN_PWD);
@@ -63,13 +62,13 @@ test.describe(() => {
       invite = await waitMessage(page, email, 40);
     });
 
-    test(qase(28, 'ON-1 Sign up with an email address'), async ({ page }) => {
+    test(qase(28, 'Sign up with an email address'), async ({ page }) => {
       await checkRegisterText(invite.inviteText, randomName);
       await page.goto(invite.inviteUrl);
       await dashboardPage.fillOnboardingFirstQuestions();
     });
 
-    test(qase([43, 44], 'ON-16,17 Onboarding questions flow'), async ({ page }) => {
+    test(qase([43, 44], 'Onboarding questions flow'), async ({ page }) => {
       await page.goto(invite.inviteUrl);
       await dashboardPage.fillOnboardingFirstQuestions();
       await dashboardPage.skipWhatNewsPopUp();
@@ -97,20 +96,20 @@ test.describe(() => {
     );
   });
 
-  test(qase(36, 'ON-9 Create demo account'), async () => {
+  test(qase(36, 'Create demo account'), async () => {
     await registerPage.isRegisterPageOpened();
-    await registerPage.clickOnCreateDemoAccountBtn();
+    await registerPage.clickOnCreateDemoAccountButton();
     await dashboardPage.fillOnboardingQuestions();
     await dashboardPage.isHeaderDisplayed('Projects');
   });
 
-  test(qase(54, 'ON-27 Sign up with email of existing user'), async () => {
+  test(qase(54, 'Sign up with email of existing user'), async () => {
     const email = process.env.LOGIN_EMAIL;
     await registerPage.registerAccount('test', email, process.env.LOGIN_PWD);
     await registerPage.isRegisterEmailCorrect(email);
   });
 
-  test.afterEach(async ({ page }, testInfo) => {
+  test.afterEach(async ({}, testInfo) => {
     await updateTestResults(testInfo.status, testInfo.retry);
   });
 });
