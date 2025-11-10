731deb558cef870f9456223f1fcbcb85019e4bd7
Daniel Herrero García <daniel.herrero@kaleidos.net>
2025-11-10 11:27:02 +0100

    feat(typo-tokens): add new test creating a typography token

--- pages/workspace/tokens/tokens-page.ts
+++ pages/workspace/tokens/tokens-page.ts
@@ -1,19 +1,31 @@
 import { type Locator, type Page, expect } from '@playwright/test';
-import { MainPage } from '../main-page';
-import { SetsComponent } from './sets-component';
-import { ThemesComponent } from './themes-component';
-import { TokensComponent } from './tokens-component';
-import { ToolsComponent } from './tools-component';
+import { BaseComponent } from '@pages/base-component';
+import { MainPage } from '@pages/workspace/main-page';
+import { SetsComponent } from '@pages/workspace/tokens/sets-component';
+import { ThemesComponent } from '@pages/workspace/tokens/themes-component';
+import { ToolsComponent } from '@pages/workspace/tokens/tools-component';
+import {
+  TokensComponent,
+  TokenClass,
+  MainToken,
+} from '@pages/workspace/tokens/tokens-component';
+import {
+  TypographyToken as TypographyToken,
+  TypographyTokensComponent,
+} from '@pages/workspace/tokens/typography-tokens-component';
 
-export type TokenType = {
+export interface BasicTokenData {
   name: string;
   description?: string;
-};
+}
+
 export class TokensPage extends MainPage {
   // components
+  readonly basePageComponent: BaseComponent;
   readonly setsComp: SetsComponent;
   readonly themesComp: ThemesComponent;
   readonly tokensComp: TokensComponent;
+  readonly typoTokensComp: TypographyTokensComponent;
   readonly toolsComp: ToolsComponent;
 
   // locators
@@ -22,10 +34,12 @@ export class TokensPage extends MainPage {
     super(page);
 
     // components
+    this.basePageComponent = new BaseComponent(page);
     this.setsComp = new SetsComponent(page);
     this.themesComp = new ThemesComponent(page);
     this.tokensComp = new TokensComponent(page, this);
     this.toolsComp = new ToolsComponent(page);
+    this.typoTokensComp = new TypographyTokensComponent(page);
 
     // locators
     this.tokensTab = page.getByRole('tab', { name: 'Tokens' });
@@ -33,5 +47,39 @@ export class TokensPage extends MainPage {
 
   async clickTokensTab() {
     await this.tokensTab.click();
+  }
+
+  private async getAddTokenButton(tokenClass: TokenClass): Promise<Locator> {
+    return this.page.getByRole('button', { name: `Add token: ${tokenClass}` });
+  }
+
+  private async createTokenViaAddButton(
+    token: TypographyToken<TokenClass> | MainToken<TokenClass>,
+  ) {
+    const addTokenButton = await this.getAddTokenButton(token.class);
+    await addTokenButton.click();
+    await this.tokensComp.tokenNameInput.fill(token.name);
+    await this.tokensComp.tokenDescriptionInput.fill(token.description ?? '');
+
+    if (token.class === TokenClass.Typography) {
+      await this.typoTokensComp.fillTokenData(token);
+    } else {
+      await this.tokensComp.fillTokenData(token);
+    }
+  }
+
+  public async createTokenViaAddButtonAndSave(
+    token: TypographyToken<TokenClass> | MainToken<TokenClass>,
+  ) {
+    await this.createTokenViaAddButton(token);
+    await this.basePageComponent.modalSaveButton.click();
+  }
+
+  public async createTokenViaAddButtonAndEnter(
+    token: TypographyToken<TokenClass> | MainToken<TokenClass>,
+  ) {
+    await this.createTokenViaAddButton(token);
+    await expect(this.modalSaveButton).toBeEnabled();
+    await this.clickOnEnter();
   }
 }
