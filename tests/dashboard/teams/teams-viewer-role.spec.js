3d184e697c4d5b0896f2c69878fbcfcfa051d52b
lugarbos <lucia.garcia@kaleidos.net>
2025-11-13 15:56:55 +0100

    refactor(tests): refactor viewer-role tests

--- tests/dashboard/teams/teams-viewer-role.spec.js
+++ tests/dashboard/teams/teams-viewer-role.spec.js
@@ -1,158 +1,136 @@
-const { mainTest, registerTest } = require('../../../fixtures');
+const { mainTest } = require('../../../fixtures');
 const { LoginPage } = require('../../../pages/login-page.js');
 const { RegisterPage } = require('../../../pages/register-page.js');
 const { ProfilePage } = require('../../../pages/profile-page.js');
 const { DashboardPage } = require('../../../pages/dashboard/dashboard-page.js');
 const { TeamPage } = require('../../../pages/dashboard/team-page.js');
 const { MainPage } = require('../../../pages/workspace/main-page.js');
-const { ViewModePage } = require('../../../pages/workspace/view-mode-page.js');
 const { LayersPanelPage } = require('../../../pages/workspace/layers-panel-page.js');
 const { random } = require('../../../helpers/string-generator.js');
 const { qase } = require('playwright-qase-reporter/playwright');
 const { expect } = require('@playwright/test');
-const {
-  getRegisterMessage,
-  checkInviteText,
-  checkMessagesCount,
-  waitMessage,
-  waitSecondMessage,
-  waitRequestMessage,
-  checkConfirmAccessText,
-  checkDashboardConfirmAccessText,
-  checkYourPenpotConfirmAccessText,
-  checkYourPenpotViewModeConfirmAccessText,
-  checkSigningText,
-} = require('../../../helpers/gmail.js');
+const { waitMessage } = require('../../../helpers/gmail.js');
 
 const maxDiffPixelRatio = 0.001;
 
-let teamPage,
-  loginPage,
-  registerPage,
-  dashboardPage,
-  layersPanelPage,
-  profilePage,
-  mainPage;
+// Set up Viewer user
+async function setupViewerUser(page, role = 'Viewer') {
+  const teamName = `${random()}-viewer-role-autotest`;
+  const userName = `${random()}-viewer-role-autotest`;
+  const userEmail = `${process.env.GMAIL_NAME}+${userName}${process.env.GMAIL_DOMAIN}`;
 
-mainTest.beforeEach(async ({ page }) => {
-  teamPage = new TeamPage(page);
-  loginPage = new LoginPage(page);
-  registerPage = new RegisterPage(page);
-  dashboardPage = new DashboardPage(page);
-  layersPanelPage = new LayersPanelPage(page);
-  profilePage = new ProfilePage(page);
-  mainPage = new MainPage(page);
-});
+  const loginPage = new LoginPage(page);
+  const registerPage = new RegisterPage(page);
+  const dashboardPage = new DashboardPage(page);
+  const teamPage = new TeamPage(page);
+  const profilePage = new ProfilePage(page);
+  const mainPage = new MainPage(page);
+  const layersPanelPage = new LayersPanelPage(page);
+
+  // Create team & file
+  await teamPage.createTeam(teamName);
+  await teamPage.isTeamSelected(teamName);
+  await dashboardPage.createFileViaPlaceholder();
+  await mainPage.waitForViewportVisible();
+  await mainPage.createDefaultRectangleByCoordinates(200, 300);
+  await mainPage.createComponentViaRightClick();
+  await mainPage.waitForChangeIsSaved();
+  await mainPage.backToDashboardFromFileEditor();
+
+  // Invite user
+  await teamPage.openInvitationsPageViaOptionsMenu();
+  await teamPage.clickInviteMembersToTeamButton();
+  await teamPage.enterEmailToInviteMembersPopUp(userEmail);
+  await teamPage.selectInvitationRoleInPopUp(role);
+  await teamPage.clickSendInvitationButton();
+  await teamPage.isSuccessMessageDisplayed('Invitation sent successfully');
+
+  // Register user
+  await profilePage.logout();
+  const invite = await waitMessage(page, userEmail, 40);
+  await page.goto(invite.inviteUrl);
+  await registerPage.registerAccount(userName, userEmail, process.env.LOGIN_PWD);
+  await dashboardPage.fillOnboardingQuestions();
+  await teamPage.isTeamSelected(teamName);
+
+  return {
+    teamName,
+    userName,
+    userEmail,
+    loginPage,
+    registerPage,
+    dashboardPage,
+    teamPage,
+    profilePage,
+    mainPage,
+    layersPanelPage,
+  };
+}
 
 mainTest.describe('Viewer Role - Permissions', () => {
-  const team = random().concat('autotest');
-  const firstAdmin = random().concat('autotest');
-  const firstEmail = `${process.env.GMAIL_NAME}+${firstAdmin}${process.env.GMAIL_DOMAIN}`;
+  let setup;
 
-  mainTest.beforeEach(
-    `Create team: "${team}", create a file, invite "${firstEmail}" to the team (Viewer role), accept invite and login with "${firstEmail}"`,
-    async ({ page }) => {
-      await mainTest.slow();
-      await teamPage.createTeam(team);
-      await teamPage.isTeamSelected(team);
-      await dashboardPage.createFileViaPlaceholder();
-      await mainPage.waitForViewportVisible();
-      await mainPage.createDefaultRectangleByCoordinates(200, 300);
-      await mainPage.createComponentViaRightClick();
-      await mainPage.waitForChangeIsSaved();
-      await mainPage.backToDashboardFromFileEditor();
+  mainTest.beforeEach(async ({ page }) => {
+    setup = await setupViewerUser(page);
+  });
 
-      await teamPage.openInvitationsPageViaOptionsMenu();
-      await teamPage.clickInviteMembersToTeamButton();
-      await teamPage.isInviteMembersPopUpHeaderDisplayed(
-        'Invite members to the team',
-      );
-      await teamPage.enterEmailToInviteMembersPopUp(firstEmail);
-      await teamPage.selectInvitationRoleInPopUp('Viewer');
-      await teamPage.clickSendInvitationButton();
-      await teamPage.isSuccessMessageDisplayed('Invitation sent successfully');
-
-      await profilePage.logout();
-      await loginPage.isLoginPageOpened();
-
-      const firstInvite = await waitMessage(page, firstEmail, 40);
-
-      await page.context().clearCookies();
-      await loginPage.reloadPage();
-      await loginPage.acceptCookie();
-
-      await page.goto(firstInvite.inviteUrl);
-      await registerPage.registerAccount(
-        firstAdmin,
-        firstEmail,
-        process.env.LOGIN_PWD,
-      );
-      await dashboardPage.isHeaderDisplayed('Projects');
-      await dashboardPage.fillOnboardingQuestions();
-      await teamPage.isTeamSelected(team);
-    },
-  );
-
-  mainTest(qase(1870, 'As a viewer user try to edit any layer'), async () => {
+  mainTest(qase(1870, 'Viewer cannot edit layers'), async () => {
+    const { dashboardPage, mainPage, layersPanelPage } = setup;
     await dashboardPage.openFileWithName('New File 1');
     await mainPage.waitForViewportVisible();
     await mainPage.isDesignTabVisible(false);
+
     await layersPanelPage.clickMainComponentOnLayersTab();
     await expect(mainPage.fileRightSidebarAside).toHaveScreenshot(
       'right-sidebar-image.png',
       {
-        maxDiffPixelRatio: maxDiffPixelRatio,
+        maxDiffPixelRatio,
         mask: [mainPage.usersSection],
       },
     );
+
     await mainPage.backToDashboardFromFileEditor();
   });
 
-  mainTest(qase(1873, 'As a viewer user import file to Drafts'), async () => {
+  mainTest(qase(1873, 'Viewer cannot import to Drafts'), async () => {
+    const { dashboardPage } = setup;
     await dashboardPage.openSidebarItem('Drafts');
     await dashboardPage.isCreateFileOnDraftsTabButtonVisible(false);
     await dashboardPage.isOptionButtonFromDraftPageVisible(false);
   });
 
-  mainTest(
-    qase(1877, 'As a viewer user Add font on  Dashboard > Fonts page'),
-    async () => {
-      await dashboardPage.openSidebarItem('Fonts');
-      await dashboardPage.isAddCustomFontButtonVisible(false);
-    },
-  );
+  mainTest(qase(1877, 'Viewer cannot add font'), async () => {
+    const { dashboardPage } = setup;
+    await dashboardPage.openSidebarItem('Fonts');
+    await dashboardPage.isAddCustomFontButtonVisible(false);
+  });
 
-  mainTest(
-    qase(
-      1880,
-      'As a viewer user check the "Create" buttons of Project and draft pages',
-    ),
-    async () => {
-      await dashboardPage.openSidebarItem('Projects');
-      await dashboardPage.isAddProjectButtonVisible(false);
-      await dashboardPage.openSidebarItem('Drafts');
-      await dashboardPage.isCreateFileOnDraftsTabButtonVisible(false);
-    },
-  );
+  mainTest(qase(1880, 'Viewer cannot create projects or drafts'), async () => {
+    const { dashboardPage } = setup;
+    await dashboardPage.openSidebarItem('Projects');
+    await dashboardPage.isAddProjectButtonVisible(false);
+    await dashboardPage.openSidebarItem('Drafts');
+    await dashboardPage.isCreateFileOnDraftsTabButtonVisible(false);
+  });
 
-  mainTest(qase(1889, 'As a viewer user try to use a toolbar'), async () => {
+  mainTest(qase(1889, 'Viewer cannot use toolbar'), async () => {
+    const { dashboardPage, mainPage } = setup;
     await dashboardPage.openFileWithName('New File 1');
     await mainPage.waitForViewportVisible();
     await mainPage.isToolBarVisible(false);
     await mainPage.backToDashboardFromFileEditor();
   });
 
-  mainTest(
-    qase(1891, 'As a viewer user try to create, duplicate and delete page'),
-    async () => {
-      await dashboardPage.openFileWithName('New File 1');
-      await mainPage.waitForViewportVisible();
-      await mainPage.isPageRightClickMenuVisible(false);
-      await mainPage.backToDashboardFromFileEditor();
-    },
-  );
+  mainTest(qase(1891, 'Viewer cannot create, duplicate, delete page'), async () => {
+    const { dashboardPage, mainPage } = setup;
+    await dashboardPage.openFileWithName('New File 1');
+    await mainPage.waitForViewportVisible();
+    await mainPage.isPageRightClickMenuVisible(false);
+    await mainPage.backToDashboardFromFileEditor();
+  });
 
-  mainTest(qase(1894, 'As a viewer user right-click created layer'), async () => {
+  mainTest(qase(1894, 'Viewer right-click menu'), async () => {
+    const { dashboardPage, mainPage, layersPanelPage } = setup;
     await dashboardPage.openFileWithName('New File 1');
     await mainPage.waitForViewportVisible();
     await layersPanelPage.clickMainComponentOnLayersTab();
@@ -160,54 +138,65 @@ mainTest.describe('Viewer Role - Permissions', () => {
     await mainPage.backToDashboardFromFileEditor();
   });
 
-  mainTest(qase(1898, 'As a viewer user try to open color palette'), async () => {
+  mainTest(qase(1898, 'Viewer cannot open color palette'), async () => {
+    const { dashboardPage, mainPage } = setup;
     await dashboardPage.openFileWithName('New File 1');
     await mainPage.waitForViewportVisible();
     await mainPage.isColorsPaletteButtonVisible(false);
     await mainPage.backToDashboardFromFileEditor();
   });
 
-  mainTest(qase(1906, 'As a viewer user try to open typographies'), async () => {
+  mainTest(qase(1906, 'Viewer cannot open typographies'), async () => {
+    const { dashboardPage, mainPage } = setup;
     await dashboardPage.openFileWithName('New File 1');
     await mainPage.waitForViewportVisible();
     await mainPage.isTypographyButtonVisible(false);
     await mainPage.backToDashboardFromFileEditor();
   });
+});
 
+mainTest.describe('Viewer Role - Role Changes', () => {
   mainTest(
     qase(
       1867,
       'Change a role of viewer user to editor and admin after accepting an invitation',
     ),
-    async () => {
+    async ({ page }) => {
+      const setup = await setupViewerUser(page);
+      const teamName = setup.teamName;
+      const {
+        dashboardPage,
+        mainPage,
+        teamPage,
+        profilePage,
+        loginPage,
+        userEmail,
+        userName,
+      } = setup;
+
+      // Viewer cannot edit initially
       await dashboardPage.openFileWithName('New File 1');
       await mainPage.waitForViewportVisible();
       await mainPage.isDesignTabVisible(false);
-
       await mainPage.backToDashboardFromFileEditor();
+
+      // Change role to Editor
       await profilePage.logout();
-      await loginPage.isLoginPageOpened();
       await loginPage.enterEmail(process.env.LOGIN_EMAIL);
       await loginPage.enterPwd(process.env.LOGIN_PWD);
       await loginPage.clickLoginButton();
       await dashboardPage.isDashboardOpenedAfterLogin();
-      await teamPage.switchTeam(team);
-      await teamPage.isTeamSelected(team);
+      await teamPage.switchTeam(teamName);
       await teamPage.openMembersPageViaOptionsMenu();
-      await teamPage.selectMemberRoleInPopUp(firstAdmin, 'Editor');
-      await teamPage.isMultipleMemberRecordDisplayed(
-        firstAdmin,
-        firstEmail,
-        'Editor',
-      );
+      await teamPage.selectMemberRoleInPopUp(userName, 'Editor');
+
+      // Verify Editor rights
       await profilePage.logout();
-      await loginPage.isLoginPageOpened();
-      await loginPage.enterEmail(firstEmail);
+      await loginPage.enterEmail(userEmail);
       await loginPage.enterPwd(process.env.LOGIN_PWD);
       await loginPage.clickLoginButton();
       await dashboardPage.isDashboardOpenedAfterLogin();
-      await teamPage.switchTeam(team);
-      await teamPage.isTeamSelected(team);
+      await teamPage.switchTeam(teamName);
       await dashboardPage.openFileWithName('New File 1');
       await mainPage.waitForViewportVisible();
       await mainPage.isDesignTabVisible(true);
@@ -215,40 +204,23 @@ mainTest.describe('Viewer Role - Permissions', () => {
     },
   );
 
-  mainTest.afterEach(
-    `Logout, login with main account, switch to to ${team} and delete it`,
-    async () => {
-      await profilePage.logout();
-      await loginPage.isLoginPageOpened();
-      await loginPage.enterEmail(process.env.LOGIN_EMAIL);
-      await loginPage.enterPwd(process.env.LOGIN_PWD);
-      await loginPage.clickLoginButton();
-      await dashboardPage.isDashboardOpenedAfterLogin();
-      await teamPage.switchTeam(team);
-      await teamPage.isTeamSelected(team);
-      await teamPage.deleteTeam(team);
-    },
-  );
-});
-
-mainTest.describe('Viewer Role - As Admin - Change to Viewer role', () => {
-  const team = random().concat('autotest');
-  const firstAdmin = random().concat('autotest');
-  const firstEmail = `${process.env.GMAIL_NAME}+${firstAdmin}${process.env.GMAIL_DOMAIN}`;
-  let profilePage, dashboardPage, loginPage, teamPage, registerPage, mainPage;
-
   mainTest(
     qase(1869, 'Change a role of admin to viewer after accepting an invitation'),
     async ({ page }) => {
-      await mainTest.slow();
-      profilePage = new ProfilePage(page);
-      dashboardPage = new DashboardPage(page);
-      loginPage = new LoginPage(page);
-      teamPage = new TeamPage(page);
-      registerPage = new RegisterPage(page);
-      mainPage = new MainPage(page);
-      await teamPage.createTeam(team);
-      await teamPage.isTeamSelected(team);
+      const teamName = `${random()}-viewer-role-autotest`;
+      const adminName = `${random()}-viewer-role-autotest`;
+      const adminEmail = `${process.env.GMAIL_NAME}+${adminName}${process.env.GMAIL_DOMAIN}`;
+
+      const loginPage = new LoginPage(page);
+      const registerPage = new RegisterPage(page);
+      const dashboardPage = new DashboardPage(page);
+      const teamPage = new TeamPage(page);
+      const profilePage = new ProfilePage(page);
+      const mainPage = new MainPage(page);
+
+      // Create team & file
+      await teamPage.createTeam(teamName);
+      await teamPage.isTeamSelected(teamName);
       await dashboardPage.createFileViaPlaceholder();
       await mainPage.waitForViewportVisible();
       await mainPage.createDefaultRectangleByCoordinates(200, 300);
@@ -256,77 +228,46 @@ mainTest.describe('Viewer Role - As Admin - Change to Viewer role', () => {
       await mainPage.waitForChangeIsSaved();
       await mainPage.backToDashboardFromFileEditor();
 
+      // Invite Admin
       await teamPage.openInvitationsPageViaOptionsMenu();
       await teamPage.clickInviteMembersToTeamButton();
-      await teamPage.isInviteMembersPopUpHeaderDisplayed(
-        'Invite members to the team',
-      );
-      await teamPage.enterEmailToInviteMembersPopUp(firstEmail);
+      await teamPage.enterEmailToInviteMembersPopUp(adminEmail);
       await teamPage.selectInvitationRoleInPopUp('Admin');
       await teamPage.clickSendInvitationButton();
-      await teamPage.isSuccessMessageDisplayed('Invitation sent successfully');
 
-      const firstInvite = await waitMessage(page, firstEmail, 40);
+      await page.context().clearCookies();
 
-      await profilePage.logout();
-      await loginPage.isLoginPageOpened();
-
-      await page.goto(firstInvite.inviteUrl);
+      const invite = await waitMessage(page, adminEmail, 60);
+      await page.goto(invite.inviteUrl);
       await registerPage.registerAccount(
-        firstAdmin,
-        firstEmail,
+        adminName,
+        adminEmail,
         process.env.LOGIN_PWD,
       );
       await dashboardPage.fillOnboardingQuestions();
-      await teamPage.isTeamSelected(team);
+      await teamPage.isTeamSelected(teamName);
 
-      await dashboardPage.openFileWithName('New File 1');
-      await mainPage.waitForViewportVisible();
-      await mainPage.isDesignTabVisible(true);
-
-      await mainPage.backToDashboardFromFileEditor();
+      // Change Admin → Viewer
       await profilePage.logout();
-      await loginPage.isLoginPageOpened();
       await loginPage.enterEmail(process.env.LOGIN_EMAIL);
       await loginPage.enterPwd(process.env.LOGIN_PWD);
       await loginPage.clickLoginButton();
       await dashboardPage.isDashboardOpenedAfterLogin();
-      await teamPage.switchTeam(team);
-      await teamPage.isTeamSelected(team);
+      await teamPage.switchTeam(teamName);
       await teamPage.openMembersPageViaOptionsMenu();
-      await teamPage.selectMemberRoleInPopUp(firstAdmin, 'Viewer');
-      await teamPage.isMultipleMemberRecordDisplayed(
-        firstAdmin,
-        firstEmail,
-        'Viewer',
-      );
+      await teamPage.selectMemberRoleInPopUp(adminName, 'Viewer');
+
+      // Verify Viewer rights
       await profilePage.logout();
-      await loginPage.isLoginPageOpened();
-      await loginPage.enterEmail(firstEmail);
+      await loginPage.enterEmail(adminEmail);
       await loginPage.enterPwd(process.env.LOGIN_PWD);
       await loginPage.clickLoginButton();
       await dashboardPage.isDashboardOpenedAfterLogin();
-      await teamPage.switchTeam(team);
-      await teamPage.isTeamSelected(team);
+      await teamPage.switchTeam(teamName);
       await dashboardPage.openFileWithName('New File 1');
       await mainPage.waitForViewportVisible();
       await mainPage.isDesignTabVisible(false);
-    },
-  );
-
-  mainTest.afterEach(
-    `Back to dashboard from file, logout, login with main account, switch to to "${team}" and delete it`,
-    async () => {
       await mainPage.backToDashboardFromFileEditor();
-      await profilePage.logout();
-      await loginPage.isLoginPageOpened();
-      await loginPage.enterEmail(process.env.LOGIN_EMAIL);
-      await loginPage.enterPwd(process.env.LOGIN_PWD);
-      await loginPage.clickLoginButton();
-      await dashboardPage.isDashboardOpenedAfterLogin();
-      await teamPage.switchTeam(team);
-      await teamPage.isTeamSelected(team);
-      await teamPage.deleteTeam(team);
     },
   );
 });
