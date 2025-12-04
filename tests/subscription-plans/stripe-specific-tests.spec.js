b4f068e383637776d6a0df9e6fede9630b075f1b
Estefania Fdez <estefafdez@gmail.com>
2025-12-04 12:43:57 +0100

    test(subscription-plans): remove skip

--- tests/subscription-plans/stripe-specific-tests.spec.js
+++ tests/subscription-plans/stripe-specific-tests.spec.js
@@ -38,7 +38,7 @@ registerTest.afterEach(async () => {
   await teamPage.deleteTeam(teamName);
 });
 
-registerTest.describe.skip(
+registerTest.describe(
   'Disabled: Flaky Stripe payment tests - awaiting stable testing strategy',
   () => {
     let testClockId, penpotId, customerData;
