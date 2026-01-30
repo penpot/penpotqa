5d9a185873f67a309cd78092d3e463b1d8b5ab59
teresa.delatorre <teresa.delatorre@kaleidos.net>
2026-01-30 08:24:33 +0100

    chore(refactor): login spec

--- tests/login-signup/login.spec.ts
+++ tests/login-signup/login.spec.ts
@@ -4,10 +4,16 @@ import { DashboardPage } from '@pages/dashboard/dashboard-page';
 import { LoginPage } from '@pages/login-page';
 import { updateTestResults } from 'helpers/saveTestResults';
 
-test(qase(35, 'Login with an email address'), async ({ page }) => {
-  const loginPage = new LoginPage(page);
+let loginPage: LoginPage;
+
+test.beforeEach('Go to login and accept cookie', async ({ page }) => {
+  loginPage = new LoginPage(page);
+
   await loginPage.goto();
   await loginPage.acceptCookie();
+});
+
+test(qase(35, 'Login with an email address'), async ({ page }) => {
   await loginPage.enterEmailAndClickOnContinue(process.env.LOGIN_EMAIL);
   await loginPage.enterPwd(process.env.LOGIN_PWD);
   await loginPage.clickLoginButton();
@@ -16,9 +22,6 @@ test(qase(35, 'Login with an email address'), async ({ page }) => {
 });
 
 test(qase(40, 'Login with invalid email address'), async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
-  await loginPage.acceptCookie();
   await loginPage.enterEmail('test@com');
   await loginPage.isEmailInputErrorDisplayed('Enter a valid email please');
   await loginPage.isLoginButtonDisplayed();
@@ -26,9 +29,6 @@ test(qase(40, 'Login with invalid email address'), async ({ page }) => {
 });
 
 test(qase(41, 'Login with no password'), async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
-  await loginPage.acceptCookie();
   await loginPage.enterEmailAndClickOnContinue(process.env.LOGIN_EMAIL);
   await loginPage.clickPwdInput();
   await loginPage.clickHeader();
@@ -37,8 +37,6 @@ test(qase(41, 'Login with no password'), async ({ page }) => {
 });
 
 test(qase(42, 'Login with incorrect password'), async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
   await loginPage.enterEmailAndClickOnContinue(process.env.LOGIN_EMAIL);
   await loginPage.enterPwd('11223344');
   await loginPage.clickLoginButton();
@@ -51,9 +49,6 @@ test(
     'Attempt to log in with a Custom SSO email using standard Penpot login form',
   ),
   async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.goto();
-    await loginPage.acceptCookie();
     await loginPage.enterEmailAndClickOnContinue(process.env.LOGIN_EMAIL);
     await loginPage.enterEmail(process.env.SSO_LOGIN_EMAIL);
     await loginPage.enterPwd(process.env.SSO_LOGIN_PWD);
