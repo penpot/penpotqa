a1b339259195536e7eb0ae6b64d571f3b047cba1
Lucía García <lucia.garcia@kaleidos.net>
2026-02-09 04:02:22 -0800

    fix(new-render): update tests and snapshots in tokens (#399)
    * fix(new-render): update tests and snapshots  in tokens
    
    * fix(new-render): update board spacing snapshot
    
    * fix(new-render): fix test

--- tests/tokens/object-specific.spec.ts
+++ tests/tokens/object-specific.spec.ts
@@ -5,6 +5,7 @@ import { MainPage } from '../../pages/workspace/main-page';
 import { TeamPage } from '../../pages/dashboard/team-page';
 import { DashboardPage } from '../../pages/dashboard/dashboard-page';
 import { TokensPage } from '../../pages/workspace/tokens/tokens-base-page';
+import { LayersPanelPage } from '@pages/workspace/layers-panel-page';
 
 const teamName = random().concat('autotest');
 
@@ -28,11 +29,13 @@ mainTest.describe(() => {
   let mainPage: MainPage;
   let dashboardPage: DashboardPage;
   let tokensPage: TokensPage;
+  let layersPanelPage: LayersPanelPage;
 
   mainTest.beforeEach(async ({ page }) => {
     mainPage = new MainPage(page);
     dashboardPage = new DashboardPage(page);
     tokensPage = new TokensPage(page);
+    layersPanelPage = new LayersPanelPage(page);
 
     await dashboardPage.createFileViaPlaceholder();
     await mainPage.isMainPageLoaded();
@@ -59,7 +62,9 @@ mainTest.describe(() => {
     await tokensPage.tokensComp.isTokenDisabledWithName('STROKE-WIDTH-10', false);
 
     await tokensPage.tokensComp.isMenuItemVisible('COLOR-1', 'Fill');
+    await layersPanelPage.openLayersTab();
     await mainPage.clickOnLayerOnCanvas();
+    await tokensPage.clickTokensTab();
     await tokensPage.tokensComp.isMenuItemVisible('COLOR-1', 'Stroke');
   });
 
