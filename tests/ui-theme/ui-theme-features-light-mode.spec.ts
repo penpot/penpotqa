0bae60ee835c2f4fd128086566f2a3357cd021e6
lugarbos <lucia.garcia@kaleidos.net>
2026-02-19 11:52:02 +0100

    fix(tests): refactor spec file to simplify and update snapshot

--- tests/ui-theme/ui-theme-features-light-mode.spec.ts
+++ tests/ui-theme/ui-theme-features-light-mode.spec.ts
@@ -1,5 +1,5 @@
 import { registerTest } from '../../fixtures';
-import { expect, test, Page } from '@playwright/test';
+import { expect, Page } from '@playwright/test';
 import { qase } from 'playwright-qase-reporter';
 import { MainPage } from '../../pages/workspace/main-page';
 import { random } from '../../helpers/string-generator';
@@ -7,38 +7,33 @@ import { ProfilePage } from '../../pages/profile-page';
 import { DashboardPage } from '../../pages/dashboard/dashboard-page';
 import { TeamPage } from '../../pages/dashboard/team-page';
 
-let profilePage: ProfilePage;
-let teamPage: TeamPage;
-let dashboardPage: DashboardPage;
-let mainPage: MainPage;
-
 const teamName: string = random().concat('autotest');
 
-test.beforeEach(async ({ page }: { page: Page }) => {
-  profilePage = new ProfilePage(page);
-  teamPage = new TeamPage(page);
-  dashboardPage = new DashboardPage(page);
-  mainPage = new MainPage(page);
+registerTest(
+  qase(1677, 'Check Light UI them in Projects page'),
+  async ({ page }: { page: Page }) => {
+    let teamPage: TeamPage = new TeamPage(page);
+    let profilePage: ProfilePage = new ProfilePage(page);
+    let dashboardPage: DashboardPage = new DashboardPage(page);
+    let mainPage: MainPage = new MainPage(page);
 
-  await teamPage.createTeam(teamName);
-  await teamPage.isTeamSelected(teamName);
-  await profilePage.openYourAccountPage();
-  await profilePage.openSettingsTab();
-  await profilePage.selectLightTheme();
-});
+    await teamPage.createTeam(teamName);
+    await teamPage.isTeamSelected(teamName);
+    await profilePage.openYourAccountPage();
+    await profilePage.openSettingsTab();
+    await profilePage.selectLightTheme();
+    await profilePage.backToDashboardFromAccount();
+    await dashboardPage.hideLibrariesAndTemplatesCarrousel();
+    await dashboardPage.createFileViaPlaceholder();
+    await mainPage.isMainPageLoaded();
+    await mainPage.backToDashboardFromFileEditor();
 
-registerTest(qase(1677, 'Check Light UI them in Projects page'), async ({}) => {
-  await profilePage.backToDashboardFromAccount();
-  await dashboardPage.hideLibrariesAndTemplatesCarrousel();
-  await dashboardPage.createFileViaPlaceholder();
-  await mainPage.isMainPageLoaded();
-  await mainPage.backToDashboardFromFileEditor();
-
-  await expect(dashboardPage.dashboardSection).toHaveScreenshot(
-    'dashboard-image.png',
-    {
-      mask: [profilePage.profileMenuButton, teamPage.teamCurrentNameDiv],
-      maxDiffPixelRatio: 0.01,
-    },
-  );
-});
+    await expect(dashboardPage.dashboardSection).toHaveScreenshot(
+      'dashboard-image.png',
+      {
+        mask: [profilePage.profileMenuButton, teamPage.teamCurrentNameDiv],
+        maxDiffPixelRatio: 0.01,
+      },
+    );
+  },
+);
