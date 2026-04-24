c0e12eb65b90c87eaecd27c7ce84e5d155ff2be1
Estefania Fdez <estefafdez@gmail.com>
2026-04-24 12:51:15 +0200

    feat(main-page): add keyboard shortcut for pressing 'B'

--- pages/workspace/main-page.js
+++ pages/workspace/main-page.js
@@ -572,6 +572,10 @@ exports.MainPage = class MainPage extends BasePage {
     await this.page.keyboard.press('C');
   }
 
+  async pressBKeyboardShortcut() {
+    await this.page.keyboard.press('b');
+  }
+
   async clickMainMenuButton() {
     await this.mainMenuButton.click();
     await expect(this.mainMenuList).toBeVisible();
