73d741b275f59cbd90ecf4145c16be041ea1ffd2
Estefania Fdez <estefafdez@gmail.com>
2026-04-24 13:55:22 +0200

    feat(rectangle-tool): add test for creating rectangle using shortcut R

--- tests/composition/composition-rectangle.spec.js
+++ tests/composition/composition-rectangle.spec.js
@@ -368,6 +368,26 @@ mainTest.describe(() => {
   });
 });
 
+mainTest(qase([275], 'Create Rectangle (Shortcut R)'), async () => {
+  await mainTest.step(
+    'Press R shortcut and verify rectangle tool is active',
+    async () => {
+      await mainPage.pressRKeyboardShortcut();
+    },
+  );
+
+  await mainTest.step(
+    'Click on canvas and verify rectangle with default size is created',
+    async () => {
+      await mainPage.clickViewportTwice();
+      await mainPage.waitForChangeIsSaved();
+      await mainPage.isCreatedLayerVisible();
+      await designPanelPage.checkSizeWidth('100');
+      await designPanelPage.checkSizeHeight('100');
+    },
+  );
+});
+
 mainTest(qase([2255], 'Select and deselect rectangles'), async ({ browserName }) => {
   await mainPage.createDefaultRectangleByCoordinates(400, 800);
   await mainPage.createDefaultRectangleByCoordinates(400, 200, true);
