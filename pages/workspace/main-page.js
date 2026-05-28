8392d69959d6126e3dd81b93995ea3761f8e0289
lugarbos <lucia.garcia@kaleidos.net>
2026-05-28 11:08:29 +0200

    feat(main-menu): refactor create text method to reduce flakiness

--- pages/workspace/main-page.js
+++ pages/workspace/main-page.js
@@ -1413,7 +1413,9 @@ exports.MainPage = class MainPage extends BasePage {
     await this.clickCreateTextButton();
     await this.clickViewportByCoordinates(x, y);
     await expect(this.textbox).toBeVisible();
-    await this.page.keyboard.type(text);
+    await expect(this.textbox).toBeFocused();
+    await this.page.keyboard.type(text, { delay: 50 });
+    await expect(this.textbox).toHaveText(text);
     await this.clickMoveButton();
     await this.waitForChangeIsSaved();
   }
