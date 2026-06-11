f91ac707f94bf9d535d7b09201ccb1ff98aa669c
lugarbos <lucia.garcia@kaleidos.net>
2026-06-11 09:38:47 +0200

    refactor(teardown): refactor create team name helper method

--- helpers/teams/create-team-name.ts
+++ helpers/teams/create-team-name.ts
@@ -2,9 +2,8 @@ import { random } from 'helpers/string-generator';
 
 export function createTeamName(): string {
   const runId = process.env.TEST_RUN_ID;
-
   const now = new Date();
-  const date = now.toISOString().slice(0, 10).replace(/-/g, '');
+  const date = now.toISOString().slice(2, 10).replace(/-/g, '');
 
-  return `${random()}${date}-${runId}`;
+  return `at-${random()}${date}-${runId}`;
 }
