f5a52aca33e63ca66be5e1f192b800e90d56dac4
Lucía García <lucia.garcia@kaleidos.net>
2026-06-23 09:36:35 +0200

    fix(composition-board): update snapshots and fix blur locators (#669)
    * fix(composition-board): update snapshots to add new size badge and locators for blur
    
    * feat(composition-board): refactor region locator to be exact true

--- pages/workspace/design-panel-page.js
+++ pages/workspace/design-panel-page.js
@@ -261,14 +261,15 @@ exports.DesignPanelPage = class DesignPanelPage extends BasePage {
     );
 
     //Design panel - Blur section
-    this.blurSection = page.locator(
-      '.main_ui_workspace_sidebar_options_menus_blur__element-set',
-    );
+    this.blurSection = page.getByRole('region', {
+      name: 'Blur effects',
+      exact: true,
+    });
     this.addBlurButton = this.blurSection.getByRole('button', { name: 'Add blur' });
     this.blurMoreOptions = this.blurSection.locator(
       'button[class*="blur__show-more"]',
     );
-    this.blurValueInput = this.blurSection.locator('#blur-input-sidebar');
+    this.blurValueInput = this.blurSection.getByRole('textbox');
     this.blurHideIcon = this.blurSection.getByRole('button', {
       name: 'Toggle blur',
     });
