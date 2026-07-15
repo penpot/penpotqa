66deb690521eff0035b07639adcb43aee23a2906
Estefania Fdez <estefafdez@gmail.com>
2026-07-15 10:36:30 +0200

    refactor(tests): remove obsolete steps from plugin installation test

--- tests/plugins/plugins.spec.js
+++ tests/plugins/plugins.spec.js
@@ -20,51 +20,39 @@ mainTest.beforeEach(async ({ page }) => {
   await pluginsPage.isMainPageLoaded();
 });
 
-mainTest(
-  qase([1837, 1838, 1839, 1842, 1844], 'Install, open and delete a plugin'),
-  async () => {
-    await mainTest.step(
-      '1837, Install a plugin by URL (via plugin icon in toolbar)',
-      async () => {
-        await pluginsPage.clickPluginsButton();
-        await pluginsPage.isPluginManagerModalVisible();
-        await pluginsPage.setPluginLoremIpsumUrl();
-        await pluginsPage.clickOnInstallPluginButton();
-        await pluginsPage.clickOnAllowPluginButton();
-        await pluginsPage.isInstalledPluginsCount(1);
-        await pluginsPage.isOpenPluginButtonVisible();
-      },
-    );
+mainTest(qase([1837, 1839, 1844], 'Install, open and delete a plugin'), async () => {
+  await mainTest.step(
+    '1837, Install a plugin by URL (via plugin icon in toolbar)',
+    async () => {
+      await pluginsPage.clickPluginsButton();
+      await pluginsPage.isPluginManagerModalVisible();
+      await pluginsPage.setPluginLoremIpsumUrl();
+      await pluginsPage.clickOnInstallPluginButton();
+      await pluginsPage.clickOnAllowPluginButton();
+      await pluginsPage.isInstalledPluginsCount(1);
+      await pluginsPage.isOpenPluginButtonVisible();
+    },
+  );
 
-    await mainTest.step('1838, Close the "Plugins Manager" modal', async () => {
-      await pluginsPage.clickOnESC();
-    });
+  await mainTest.step(
+    '1844, Open a plugin (via Main menu 3 dots > Plugins > plugin name)',
+    async () => {
+      await pluginsPage.clickMainMenuButton();
+      await pluginsPage.clickPluginsMainMenuItem();
+      await pluginsPage.clickLoremIpsumButton();
+      await pluginsPage.isLoremIpsumPluginVisible();
+    },
+  );
 
-    await mainTest.step(
-      '1844, Open a plugin (via Main menu 3 dots > Plugins > plugin name)',
-      async () => {
-        await pluginsPage.clickMainMenuButton();
-        await pluginsPage.clickPluginsMainMenuItem();
-        await pluginsPage.clickLoremIpsumButton();
-        await pluginsPage.isLoremIpsumPluginVisible();
-      },
-    );
-
-    await mainTest.step(
-      '1839, Delete a plugin from the "Plugins Manager" modal (via delete icon button)',
-      async () => {
-        await pluginsPage.clickMainMenuButton();
-        await pluginsPage.clickPluginsMainMenuItem();
-        await pluginsPage.clickPluginsManagerButton();
-        await pluginsPage.clickOnDeletePluginButton();
-        await pluginsPage.isInstalledPluginsCount(0);
-        await pluginsPage.isNoPluginMessageVisible();
-      },
-    );
-
-    await mainTest.step('1842, Close the plugin modal (via "X" icon)', async () => {
-      await pluginsPage.clickClosePluginPanelButton();
-      await pluginsPage.isPluginManagerModalNotVisible();
-    });
-  },
-);
+  await mainTest.step(
+    '1839, Delete a plugin from the "Plugins Manager" modal (via delete icon button)',
+    async () => {
+      await pluginsPage.clickMainMenuButton();
+      await pluginsPage.clickPluginsMainMenuItem();
+      await pluginsPage.clickPluginsManagerButton();
+      await pluginsPage.clickOnDeletePluginButton();
+      await pluginsPage.isInstalledPluginsCount(0);
+      await pluginsPage.isNoPluginMessageVisible();
+    },
+  );
+});
