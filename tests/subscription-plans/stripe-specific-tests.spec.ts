9b22949d0b03e6dc0fbe63f28f3f18f7d3ea924d
Teresa de la Torre <teresa.delatorre@kaleidos.net>
2026-08-26 12:39:31 +0200

    chore(subscriptions): skip some tests to update adding comments (#836)

--- tests/subscription-plans/stripe-specific-tests.spec.ts
+++ tests/subscription-plans/stripe-specific-tests.spec.ts
@@ -43,7 +43,8 @@ registerTest.describe(() => {
     await profilePage.backToDashboardFromAccount();
   });
 
-  registerTest(
+  // TODO: Re-do following the updated test case in Qase
+  registerTest.skip(
     qase(2346, 'Invoices capped at $7 (Unlimited)'),
     async ({ email }) => {
       const currentPlan = 'Unlimited';
@@ -78,35 +79,39 @@ registerTest.describe(() => {
     },
   );
 
-  registerTest(qase(2514, 'Maximum billing $175 (Unlimited)'), async ({ email }) => {
-    const currentPlan = 'Unlimited';
-    const date = new Date();
+  // TODO: Re-do following the updated test case in Qase (now simplified)
+  registerTest.skip(
+    qase(2514, 'Maximum billing $175 (Unlimited)'),
+    async ({ email }) => {
+      const currentPlan = 'Unlimited';
+      const date = new Date();
 
-    await profilePage.tryTrialForPlan(currentPlan, '100');
-    await profilePage.openYourAccountPage();
-    await profilePage.openSubscriptionTab();
-    await profilePage.clickOnAddPaymentMethodButton();
-    await addPaymentMethodForCustomer(customerData.customerId);
-    await stripePage.reloadPage();
-    await stripePage.isVisaCardAdded(true);
-    await skipSubscriptionByDays(email, testClockId, 20, date);
+      await profilePage.tryTrialForPlan(currentPlan, '100');
+      await profilePage.openYourAccountPage();
+      await profilePage.openSubscriptionTab();
+      await profilePage.clickOnAddPaymentMethodButton();
+      await addPaymentMethodForCustomer(customerData.customerId);
+      await stripePage.reloadPage();
+      await stripePage.isVisaCardAdded(true);
+      await skipSubscriptionByDays(email, testClockId, 20, date);
 
-    await stripePage.waitTrialEndsDisappear();
-    await profilePage.reloadPage();
-    await stripePage.checkCurrentSubscription(currentPlan);
-    await stripePage.checkLastInvoiceName(`Penpot ${currentPlan} (per editors)`);
-    await stripePage.checkLastInvoiceAmount(`175.00`);
+      await stripePage.waitTrialEndsDisappear();
+      await profilePage.reloadPage();
+      await stripePage.checkCurrentSubscription(currentPlan);
+      await stripePage.checkLastInvoiceName(`Penpot ${currentPlan} (per editors)`);
+      await stripePage.checkLastInvoiceAmount(`175.00`);
 
-    await skipSubscriptionByMonths(email, testClockId, 1, date);
-    await profilePage.reloadPage();
+      await skipSubscriptionByMonths(email, testClockId, 1, date);
+      await profilePage.reloadPage();
 
-    await skipSubscriptionByMonths(email, testClockId, 1, date);
-    await profilePage.reloadPage();
+      await skipSubscriptionByMonths(email, testClockId, 1, date);
+      await profilePage.reloadPage();
 
-    await skipSubscriptionByMonths(email, testClockId, 1, date);
-    await profilePage.reloadPage();
+      await skipSubscriptionByMonths(email, testClockId, 1, date);
+      await profilePage.reloadPage();
 
-    await stripePage.checkLastInvoiceStatus(`Paid`);
-    await stripePage.waitInvoiceAmountCount(`$175.00`, 3);
-  });
+      await stripePage.checkLastInvoiceStatus(`Paid`);
+      await stripePage.waitInvoiceAmountCount(`$175.00`, 3);
+    },
+  );
 });
