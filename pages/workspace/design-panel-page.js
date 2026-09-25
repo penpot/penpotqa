b112933294539a24bfbc4b130f640b62bf160e6f
Teresa de la Torre <teresa.delatorre@kaleidos.net>
2026-09-25 14:20:27 +0200

    fix(test): PENPOT-2433 (#885)

--- pages/workspace/design-panel-page.js
+++ pages/workspace/design-panel-page.js
@@ -686,7 +686,9 @@ exports.DesignPanelPage = class DesignPanelPage extends BasePage {
   }
 
   async isFillHexCodeSetComponent(value) {
-    await expect(this.componentColorInput).toHaveValue(value.slice(1));
+    await expect(this.componentColorInput).toHaveValue(value.slice(1), {
+      timeout: 30000,
+    });
   }
 
   async isFillTokenColorSetComponent(value) {
